6e5315dafc1faa39c4092781b6fbc4ec336ddbca
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 05:24:42 -0500

    [ADD] default layout

---
+++ layouts/default.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-app
+    class="pg-app"
+  >
+    <!-- APP BAR -->
+    <application-header />
+    <!-- NAV DRAWER -->
+    <!-- <v-navigation-drawer
+      v-model="appDrawer"
+      app
+      temporary
+      clipped
+      fixed
+      disable-resize-watcher
+      :mobile-break-point="600"
+      class="ff-app-drawer"
+      :class="{ 'ff-app-drawer-sm': $vuetify.breakpoint.sm }"
+    >
+      <admin-list
+        v-if="isAdminUser"
+        @item-clicked="appDrawer = false"
+      />
+      <employer-menu-item-list
+        v-else-if="isEmployerUser"
+        @item-clicked="appDrawer = false"
+      />
+      <pro-list
+        v-else-if="isProUser"
+        @item-clicked="appDrawer = false"
+      />
+      <menu-item-list :items="menuItems" />
+    </v-navigation-drawer> -->
+
+    <!-- CONTENT -->
+    <v-main>
+      <v-container
+        fluid
+        pa-0
+        class="pg-app-container"
+      >
+        <nuxt />
+      </v-container>
+    </v-main>
+    <!-- FOOTER -->
+    <v-footer
+      app
+      class="pg-app-footer"
+    >
+      <v-col
+        cols="12"
+      >
+        <span>&copy; {{ new Date().getFullYear() }} </span>
+      </v-col>
+    </v-footer>
+    <!--
+      Reusable snackbar component
+      Usage: $store.commit('app/SET_GLOBAL_SNACKBAR', { ...options }) See options in store/app/mutations
+    -->
+    <!-- <v-snackbar
+      :value="snack.show"
+      bottom
+      :color="snack.color"
+      :multi-line="snack.multiline"
+      :timeout="snack.timeout"
+      @input="toggleSnack"
+    >
+      <span class="subtitle-1">{{ snack.text }}</span>
+      <v-btn text @click.stop="snack.action">
+        {{ snack.actionText }}
+      </v-btn>
+    </v-snackbar> -->
+  </v-app>
+</template>
+
+<script>
+import ApplicationHeader from '@/components/app/menu/ApplicationHeader.vue'
+
+export default {
+  components: {
+    ApplicationHeader
+  },
+  data () {
+    return {
+      appDrawer: false,
+      title: 'Playgarden Prep',
+      menuItems: [
+        {
+          title: 'Log out',
+          click: this.logout
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pg-app {
+  height: 100%;
+  margin: 0 auto;
+  color: $pg-black;
+  &-bar {
+    z-index: 10;
+  }
+  &-container {
+    position: relative;
+    height: 100%;
+  }
+}
+</style>
