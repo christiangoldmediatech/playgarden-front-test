fad097806ee72c62d81ff2d79c208bc43730f88f
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 05:24:54 -0500

    [ADD] default page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <v-row justify="center" align="center">
+      <v-col cols="6">
+        <p>hey</p>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
