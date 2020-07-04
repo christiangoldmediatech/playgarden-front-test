dec423bd216637d7ba3edeaf3a833f8e60ef4edb
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 05:24:23 -0500

    [ADD] first commit

---
+++ vuetify.options.js
@@ -0,0 +1,17 @@
+export default function ({ app }) {
+  return {
+    theme: {
+      dark: false,
+      themes: {
+        light: {
+          primary: '#C2DAA5',
+          secondary: '#E2EBBA',
+          error: '#C61818',
+          accent: '#FEC572',
+          success: '#C5DCA6',
+          black: '#606060'
+        }
+      }
+    }
+  }
+}
