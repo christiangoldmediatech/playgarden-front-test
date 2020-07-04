f4bc53e4f18486a9911963a843e1a4896c8178a1
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 13:37:59 -0500

    [ADD] underlined components

---
+++ components/global/UnderlinedSubtitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <span
+    v-bind="$attrs"
+    class="underlined-title"
+    v-on="$listeners"
+  >
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.underlined-title {
+  font-size: 20px;
+  position: relative;
+  z-index: 1;
+  &::after {
+    width: 100%;
+    position: absolute;
+    bottom: 5px;
+    left: 0;
+    content: "";
+    z-index: -1;
+    border-bottom: 5px solid $pg-accent;
+    border-radius: 5px;
+  }
+}
+</style>
---
+++ components/global/UnderlinedTitle.vue
@@ -0,0 +1,39 @@
+<template>
+  <span
+    v-bind="$attrs"
+    class="underlined-title"
+    v-on="$listeners"
+  >
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.underlined-title {
+  font-size: 65px;
+  font-weight: bold;
+  position: relative;
+  z-index: 1;
+  &::after {
+    width: 100%;
+    position: absolute;
+    bottom: 5px;
+    left: 0;
+    content: "";
+    z-index: -1;
+    border-bottom: 30px solid $pg-main;
+    border-radius: 5px;
+  }
+}
+</style>
