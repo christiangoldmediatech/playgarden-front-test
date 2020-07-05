0b660837cff8b2b2e0aa7fa39d0dda91eff491d4
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 19:17:56 -0500

    [ADD] loading layout

---
+++ layouts/loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <!-- CONTENT -->
+    <v-main>
+      <v-container fluid fill-height>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <!-- FOOTER -->
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }} </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.logo-img {
+  padding-top: 8px;
+  width: 200px;
+}
+</style>
