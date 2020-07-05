b481e625b5897ad1d7c57942e2100c6d05b147ab
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-04 19:44:00 -0500

    [ADD] new reset password page

---
+++ pages/reset-password.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container fluid>
+    <v-row no-gutters>
+      <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="6">
+        <div class="image">
+          <img src="@/assets/svg/girl-smiling.svg" alt="Smiling Girl Picture">
+        </div>
+      </v-col>
+      <v-col cols="12" md="6">
+        <div class="form mx-auto px-4">
+          <div>
+            <underlined-title text="Reset Password" />
+          </div>
+          <p>
+            Enter your email to reset your password. You will receive an email with instructions on how to reset your password.
+          </p>
+          <p v-show="errorMessage" class="error-message">
+            <v-icon color="error">
+              mdi-close-circle
+            </v-icon>
+            {{ errorMessage }}
+          </p>
+          <reset-password-form :loading="isLoadingForm" @click:submit="handleLogin" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import UnderlinedTitle from '@/components/global/UnderlinedTitle.vue'
+import ResetPasswordForm from '@/components/forms/password/ResetPasswordForm.vue'
+
+export default {
+  middleware: ['redirectToAuthPage'],
+  components: {
+    UnderlinedTitle,
+    ResetPasswordForm
+  },
+  data () {
+    return {
+      isLoadingForm: false,
+      errorMessage: ''
+    }
+  },
+  methods: {
+    async handleLogin (user) {
+      try {
+        this.isLoadingForm = true
+        const { data } = await this.$axios.post(`${process.env.apiBaseUrl}/auth/login`, user)
+        // set auth token
+        this.$store.dispatch('auth/setToken', data.accessToken)
+        this.errorMessage = ''
+      } catch (error) {
+        this.handleLoginError(error)
+      } finally {
+        this.isLoadingForm = false
+      }
+    },
+    handleLoginError (error) {
+      this.errorMessage = 'Sorry! Wrong email or password'
+      // eslint-disable-next-line
+      console.error(error)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.image {
+  height: 100%;
+  max-height: 500px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  img {
+    height: 100%;
+  }
+}
+.form {
+  max-width: 500px;
+}
+.error-message {
+  color: $pg-error;
+}
+</style>
