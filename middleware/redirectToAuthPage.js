55cb678823a837a023afceee9e3c8ef7e00f4031
EmilianoSotomayor <emi@goldmediatech.com>
2020-07-06 02:05:07 -0500

    [FIX] use path instead of fullPath

--- middleware/redirectToAuthPage.js
+++ middleware/redirectToAuthPage.js
@@ -3,7 +3,7 @@ import unauthenticatedRoutes from '@/utils/consts/unauthenticatedRoutes.js'
 export default async function ({ app, store, redirect, route }) {
   const isLoggedIn = await store.dispatch('auth/checkAuth')
   if (!isLoggedIn) {
-    if (unauthenticatedRoutes.includes(route.fullPath)) {
+    if (unauthenticatedRoutes.some(path => path.includes(route.path))) {
       // route is an expected unauthorized path don't redirect
       return store.dispatch('auth/logout')
     } else {
