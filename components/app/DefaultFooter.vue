110898d79a12278334509f4e90e0ab7c43e75093
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-07-16 14:54:49 -0500

    [ADD] adds refactored footer

--- components/app/DefaultFooter.vue
+++ components/app/DefaultFooter.vue
@@ -1,154 +1,166 @@
 <template>
   <v-footer
+    class="mt-16"
     padless
+    tile
   >
-    <div class="green-line-1" />
-    <div class="green-line-2" />
-    <v-container class="pg-app-footer" fluid>
-      <v-row>
-        <v-col
-          class="text-center text-md-left"
-          cols="12"
-          lg="5"
-          md="4"
-          sm="6"
-          xl="4"
-        >
-          <v-container>
-            <v-row>
-              <v-col
-                cols="12"
-                lg="11"
-                xl="10"
-              >
-                <v-img
-                  alt="Playgarden Prep's Logo"
-                  class="mb-1 mt-0"
-                  contain
-                  :src="require('@/assets/svg/logo.svg')"
+    <v-card
+      flat
+      tile
+      width="100%"
+    >
+      <div class="green-line-1" />
+      <div class="green-line-2" />
+      <v-card-text class="pt-0">
+        <v-container fluid py-0>
+          <v-row>
+            <v-col
+              class="text-center text-md-left"
+              cols="12"
+              lg="5"
+              md="4"
+              sm="6"
+              xl="4"
+            >
+              <v-container py-0>
+                <v-row no-gutters>
+                  <v-col
+                    cols="12"
+                    lg="9"
+                    xl="8"
+                  >
+                    <v-img
+                      alt="Playgarden Prep's Logo"
+                      class="mb-1 mt-0"
+                      contain
+                      :src="require('@/assets/svg/logo.svg')"
+                    />
+                  </v-col>
+                </v-row>
+                <div class="align-center d-flex justify-center justify-md-start">
+                  <img
+                    alt="Facebook"
+                    class="clickable mr-1"
+                    src="@/assets/svg/social-media/facebook.svg"
+                  >
+                  <img
+                    alt="Instagram"
+                    class="clickable mx-1"
+                    src="@/assets/svg/social-media/instagram.svg"
+                  >
+                  <div class="clickable d-flex justify-center mx-1 social-media-circle">
+                    <img
+                      alt="Email"
+                      src="@/assets/svg/social-media/email.svg"
+                    >
+                  </div>
+                  <img
+                    alt="Twitter"
+                    class="clickable mx-1"
+                    src="@/assets/svg/social-media/twitter.svg"
+                  >
+                </div>
+              </v-container>
+            </v-col>
+
+            <v-col
+              class="text-center text-md-left"
+              cols="12"
+              lg="3"
+              md="2"
+              sm="6"
+              xl="2"
+            >
+              <v-container>
+                <nuxt-link
+                  class="d-block link-text"
+                  :to="{ name: 'index' }"
+                  v-text="'Playgarden Prep'"
                 />
-              </v-col>
-            </v-row>
-            <div class="align-center d-flex justify-center justify-md-start">
-              <img
-                alt="Facebook"
-                class="clickable mr-1"
-                src="@/assets/svg/social-media/facebook.svg"
-              >
-              <img
-                alt="Instagram"
-                class="clickable mx-1"
-                src="@/assets/svg/social-media/instagram.svg"
-              >
-              <div class="clickable d-flex justify-center mx-1 social-media-circle">
-                <img
-                  alt="Email"
-                  src="@/assets/svg/social-media/email.svg"
-                >
-              </div>
-              <img
-                alt="Twitter"
-                class="clickable mx-1"
-                src="@/assets/svg/social-media/twitter.svg"
-              >
-            </div>
-          </v-container>
-        </v-col>
 
-        <v-col
-          class="text-center text-md-left"
-          cols="12"
-          lg="3"
-          md="2"
-          sm="6"
-          xl="2"
-        >
-          <v-container>
-            <nuxt-link
-              class="d-block link-text"
-              :to="{ name: 'index' }"
-              v-text="'Playgarden Prep'"
-            />
+                <nuxt-link
+                  class="d-block link-text"
+                  :to="{ name: 'index' }"
+                  v-text="'Terms & Conditions'"
+                />
 
-            <nuxt-link
-              class="d-block link-text"
-              :to="{ name: 'index' }"
-              v-text="'Terms & Conditions'"
-            />
+                <nuxt-link
+                  class="d-block link-text"
+                  :to="{ name: 'index' }"
+                  v-text="'Privacy Policy'"
+                />
 
-            <nuxt-link
-              class="d-block link-text"
-              :to="{ name: 'index' }"
-              v-text="'Privacy Policy'"
-            />
+                <nuxt-link
+                  class="d-block link-text"
+                  :to="{ name: 'index' }"
+                  v-text="'Help'"
+                />
+              </v-container>
+            </v-col>
 
-            <nuxt-link
-              class="d-block link-text"
-              :to="{ name: 'index' }"
-              v-text="'Help'"
-            />
-          </v-container>
-        </v-col>
+            <v-col
+              class="text-center"
+              cols="12"
+              lg="2"
+              md="3"
+              sm="6"
+              xl="3"
+            >
+              <p class="font-weight-bold mb-1 mt-0 primary--text text-h5">
+                Upper East Side
+              </p>
 
-        <v-col
-          class="text-center"
-          cols="12"
-          lg="2"
-          md="3"
-          sm="6"
-          xl="3"
-        >
-          <p class="font-weight-bold mb-1 mt-0 primary--text text-h5">
-            Upper East Side
-          </p>
+              <p class="font-weight-bold mb-1">
+                ues@playgardenprep.com<br>
+                212-965-9718
+              </p>
 
-          <p class="font-weight-bold mb-1">
-            ues@playgardenprep.com<br>
-            212-965-9718
-          </p>
+              <p>
+                1366 Madison Avenue<br>
+                New York, NY 10128
+              </p>
+            </v-col>
 
-          <p>
-            1366 Madison Avenue<br>
-            New York, NY 10128
-          </p>
-        </v-col>
+            <v-col
+              class="text-center"
+              cols="12"
+              lg="2"
+              md="3"
+              sm="6"
+              xl="3"
+            >
+              <p class="font-weight-bold mb-1 mt-0 primary--text text-h5">
+                Tribeca
+              </p>
 
-        <v-col
-          class="text-center"
-          cols="12"
-          lg="2"
-          md="3"
-          sm="6"
-          xl="3"
-        >
-          <p class="font-weight-bold mb-1 mt-0 primary--text text-h5">
-            Tribeca
-          </p>
+              <p class="font-weight-bold mb-1">
+                tribeca@playgardenprep.com<br>
+                212-965-9717
+              </p>
 
-          <p class="font-weight-bold mb-1">
-            tribeca@playgardenprep.com<br>
-            212-965-9717
-          </p>
-
-          <p>
-            95 Franklin Street<br>
-            New York, NY 10013
-          </p>
-        </v-col>
-      </v-row>
+              <p>
+                95 Franklin Street<br>
+                New York, NY 10013
+              </p>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
 
       <v-divider class="mx-md-16 mx-sm-8" />
 
-      <v-row>
-        <v-col class="text-center">
-          2020
-          <template v-if="new Date().getFullYear() > 2020">
-            - {{ new Date().getFullYear() }}
-          </template>
-          &copy; Playgarden Prep. All rights reserved.
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-container>
+        <v-row>
+          <v-col class="text-center" cols="12">
+            2020
+            <template v-if="new Date().getFullYear() > 2020">
+              - {{ new Date().getFullYear() }}
+            </template>
+            &copy; Playgarden Prep. All rights reserved.
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </v-footer>
 </template>
 
