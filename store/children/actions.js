4ea05414d646f02429e7b50ea534c368461adf50
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-08-05 18:55:16 -0500

    [FIX] fixed more merge conflicts

--- store/children/actions.js
+++ store/children/actions.js
@@ -1,13 +1,11 @@
 export default {
-  async store ({ commit }, data) {
+  async store (ctx, data) {
     try {
       const response = await this.$axios.post('/children', data)
 
       return response.data
     } catch (error) {
-      snotifyError(commit, {
-        body: 'Sorry! There was an error while storing.'
-      })
+      return Promise.reject(error)
     }
   },
 
