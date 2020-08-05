c7d2e588caa756bb5866f5aad90332a6106c6d01
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-08-05 17:04:15 -0500

    [FIX] fixed expiration time

--- store/actions.js
+++ store/actions.js
@@ -3,8 +3,14 @@ import { hasLocalStorage } from '@/utils/window'
 export default {
   setChild ({ commit }, { value, oldExp = null, save = false }) {
     commit('SET_CURRENT_CHILD', value)
+    
+    const moment = new Date()
+    moment.setHours(23)
+    moment.setMinutes(59)
+    moment.setSeconds(59)
+    moment.setMilliseconds(999)
 
-    const expires = oldExp || new Date().getTime() + (24 * 60 * 60 * 1000)
+    const expires = oldExp || moment.getTime()
     commit('SET_CURRENT_CHILD_EXPIRES', expires)
 
     if (hasLocalStorage() && save) {
