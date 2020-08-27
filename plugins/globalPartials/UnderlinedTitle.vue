b30f6c67e4ca7b55046e74dc00859e959a3f87a6
Paola Boada <paolaboadaramos@gmail.com>
2020-08-27 13:28:53 -0300

    [UPDATE] underline title component

--- plugins/globalPartials/UnderlinedTitle.vue
+++ plugins/globalPartials/UnderlinedTitle.vue
@@ -6,6 +6,12 @@
       'text-none': $vuetify.breakpoint.mdAndUp,
       'underlined-title': $vuetify.breakpoint.mdAndUp
     }"
+    :style="{
+      '--ut-abb-color': _abbColor,
+      '--ut-abb-width': _abbFontSize,
+      fontSize: _fontSize,
+      fontWeight: _fontWeight
+    }"
     v-on="$listeners"
   >
     {{ text }}
@@ -17,9 +23,118 @@ export default {
   name: 'UnderlinedTitle',
 
   props: {
+    abbColor: {
+      type: [Object, String],
+      default: () => ({ color: 'primary', light: 'base' }),
+      validator: (value) => {
+        if (!value) {
+          return false
+        }
+
+        // using HEX or vuetify colors vars
+        if (typeof value === 'string') {
+          return true
+        }
+
+        // using vuetify color with object
+        if (!value.color || !value.light) {
+          return false
+        }
+
+        if (
+          ![
+            'primary',
+            'secondary',
+            'accent',
+            'error',
+            'info',
+            'success',
+            'warning',
+            'black'
+          ].includes(value.color)
+        ) {
+          return false
+        }
+
+        return (
+          !value.light ||
+          [
+            'base',
+            'lighten5',
+            'lighten4',
+            'lighten3',
+            'lighten2',
+            'lighten1',
+            'darken1',
+            'darken2',
+            'darken3',
+            'darken4'
+          ].includes(value.light)
+        )
+      }
+    },
+
+    abbFontSize: {
+      type: String,
+      default: '20px'
+    },
+
+    fontSize: {
+      type: String,
+      default: '50px'
+    },
+
+    fontWeight: {
+      type: [Number, String],
+      default: 'bold'
+    },
+
+    // easy shortcut for faster styling
+    // Note: it override the others options
+    subtitle: Boolean,
+
     text: {
       type: String,
       required: true
+    }
+  },
+
+  computed: {
+    _abbColor () {
+      if (this.subtitle) {
+        return 'var(--v-accent-base)'
+      }
+
+      // using HEX or vuetify colors vars
+      if (typeof this.abbColor === 'string') {
+        return this.abbColor
+      }
+
+      return `var(--v-${this.abbColor.color}-${this.abbColor.light || 'base'})`
+    },
+
+    _abbFontSize () {
+      if (this.subtitle) {
+        return '5px'
+      }
+
+      return this.abbFontSize
+    },
+
+    _fontSize () {
+      if (this.subtitle) {
+        return '18px'
+      }
+
+      return this.fontSize
+    },
+
+    _fontWeight () {
+      if (this.subtitle) {
+        return 500
+      }
+
+      return this.fontWeight
     }
   }
 }
@@ -27,11 +142,10 @@ export default {
 
 <style lang="scss" scoped>
 .underlined-title {
-  font-size: 50px;
-  font-weight: bold;
   position: relative;
   z-index: 1;
   color: $pg-black;
+
   &::after {
     width: 102%;
     position: absolute;
@@ -39,7 +153,9 @@ export default {
     left: -1%;
     content: "";
     z-index: -1;
-    border-bottom: 20px solid $pg-main;
+    border-bottom-color: var(--ut-abb-color);
+    border-bottom-style: solid;
+    border-bottom-width: var(--ut-abb-width);
     border-radius: 7px;
   }
 }
