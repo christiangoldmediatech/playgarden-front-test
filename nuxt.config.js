f1aa5a5fd925d2269cf9794443383907a92975e5
Emiliano Sotomayor <emilianosotomayor@Emilianos-MacBook-Pro.local>
2020-08-31 17:09:21 -0500

    [ADD] env test

--- nuxt.config.js
+++ nuxt.config.js
@@ -130,7 +130,10 @@ export default {
   env: {
     apiBaseUrl:
       process.env.API_BASE_URL ||
-      'https://j2ld7v9jji.execute-api.us-east-1.amazonaws.com/staging'
+      'https://j2ld7v9jji.execute-api.us-east-1.amazonaws.com/staging',
+    testEnv:
+      process.env.TEST_ENV ||
+      'LOCAL'
   },
   router: {
     middleware: [
