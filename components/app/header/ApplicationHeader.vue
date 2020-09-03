7ad89f55a0f49630242b13775f1bbd241803cf5e
uboat46 <uboat46@gmail.com>
2020-09-03 00:00:25 -0500

    Revert "Merge pull request #96 from Gold-Media-Tech/featue/style-fixes"
    This reverts commit b9d60f7492743aedd2d49a86370384516f65f228, reversing
    changes made to cc1e0bb494dc2e88dd23874ceb2a5d0da8de076d.

--- components/app/header/ApplicationHeader.vue
+++ components/app/header/ApplicationHeader.vue
@@ -37,7 +37,7 @@
 
     <div class="justify-end">
       <v-btn
-        class="px-13 ml-3 hidden-sm-and-down text-right btn-register"
+        class="px-13 ml-3 hidden-sm-and-down text-right"
         color="accent"
         nuxt
         v-if="!isUserLoggedIn"
@@ -122,10 +122,6 @@ export default {
 
 .no-border-radius {
   border-radius: 0px !important;
-}
-
-.btn-register:before {
-  background-color: transparent !important;
 }
 
 .justify-end {
