c35136050444bbb531be04c6fda948b1a4dca4e0
Paola Boada <paolaboadaramos@gmail.com>
2020-09-04 15:18:56 -0300

    [UPDATE] grey background card

--- components/app/student-cubby/LetterCard.vue
+++ components/app/student-cubby/LetterCard.vue
@@ -2,10 +2,7 @@
   <v-hover v-slot="{ hover }">
     <v-card
       class="ma-1"
-      :class="{
-        clickable: _unblocked,
-        grayscale: !_unblocked
-      }"
+      :class="{ clickable: _unblocked, 'grey lighten-2': !_unblocked }"
       :height="141"
       :width="141"
       :elevation="hover ? 9 : 3"
@@ -13,13 +10,17 @@
       <div class="align-center d-flex flex-column justify-end fill-height">
         <v-img
           class="flex-shrink-1 flex-grow-0"
+          :class="{ 'blank-shape': !_unblocked }"
           contain
           max-height="80"
           max-width="80"
           :src="letter.icon"
         />
 
-        <span class="d-block accent--text text-center font-weight-bold text-h6">
+        <span
+          class="d-block accent--text text-center font-weight-bold text-h6"
+          :class="{ 'white--text': !_unblocked }"
+        >
           {{ letter.name }}
         </span>
       </div>
