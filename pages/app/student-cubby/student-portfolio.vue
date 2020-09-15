c3a1babdba7a863c2c696c0a731d60c564802e12
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-09-15 12:49:16 -0500

    [ADD] adds no uploads screen to portfolio

--- pages/app/student-cubby/student-portfolio.vue
+++ pages/app/student-cubby/student-portfolio.vue
@@ -3,18 +3,58 @@
     <v-card-text>
       <div class="text-center">
         <underlined-title class="text-h3" text="Student Portfolio" />
-        <p class="mt-5">
+        <p class="portfolio-subtitle mt-3">
           Keep track of your worksheets and progress!
         </p>
       </div>
     </v-card-text>
 
-    <portfolio-carousel
-      v-for="category in categories"
-      :key="`portfolio-category-${category.id}`"
-      v-bind="{ category }"
-    />
-    <portfolio-overlay />
+    <template v-if="!loading && uploadedWorksheets.length === 0">
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-row align="center" justify="center">
+            <v-col cols="4">
+              <v-img
+                :src="require('@/assets/png/student-cubby/portfolio/placeholder1.png')"
+                contain
+              />
+            </v-col>
+            <v-col cols="4">
+              <v-img
+                :src="require('@/assets/png/student-cubby/portfolio/placeholder2.png')"
+                contain
+              />
+            </v-col>
+            <v-col cols="4">
+              <v-img
+                :src="require('@/assets/png/student-cubby/portfolio/placeholder3.png')"
+                contain
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="8">
+          <p class="portfolio-no-uploads">
+            You have no worksheets uploaded yet
+          </p>
+        </v-col>
+        <v-col cols="8">
+          <p class="portfolio-text">
+            Take a photo of the completed worksheets and upload them here to keep track of progress in tracing, math, science, language and cognitive activities.
+          </p>
+        </v-col>
+      </v-row>
+    </template>
+    <template v-else>
+      <portfolio-carousel
+        v-for="category in categories"
+        :key="`portfolio-category-${category.id}`"
+        v-bind="{ category }"
+      />
+      <portfolio-overlay />
+    </template>
   </v-card>
 </template>
 
@@ -33,7 +73,8 @@ export default {
 
   data: () => {
     return {
-      uploadedWorksheets: []
+      uploadedWorksheets: [],
+      loading: true
     }
   },
 
@@ -63,8 +104,27 @@ export default {
     ...mapActions('offline-worksheet', { getUploaded: 'getUploaded' }),
 
     async refresh () {
-      this.uploadedWorksheets = await this.getUploaded(this.studentId)
+      this.uploadedWorksheets = await this.getUploaded(this.studentId).then(() => {
+        this.loading = false
+      })
     }
   }
 }
 </script>
+
+<style lang="scss">
+.portfolio {
+  &-subtitle {
+    font-size: 20px;
+  }
+  &-no-uploads {
+    font-size: 28px;
+    font-weight: bold;
+    text-align: center;
+  }
+  &-text {
+   font-size: 20px;
+   text-align: center;
+  }
+}
+</style>
