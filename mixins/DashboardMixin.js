49bdb5ccb116c30dc405f417e55f988e4bb6fa69
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-09-18 22:17:09 -0500

    [FIX] fixes issue with activities

--- mixins/DashboardMixin.js
+++ mixins/DashboardMixin.js
@@ -108,7 +108,7 @@ export default {
 
       // Get items for links
       const videos = activities.map(({ id, activityType, videos, viewed }, i) => {
-        let disabled = true
+        let disabled = (this.videos.progress !== 100)
         const previous = i > 0 ? activities[i - 1] : null
         if (previous) {
           disabled = !(previous.viewed && previous.viewed.completed)
