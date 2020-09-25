abbd61ed6758ed43021dd3f641a5089fa8a37055
Christian Borja <christian@goldmediatech.com>
2020-09-24 20:25:31 -0500

    add modal dialog to portfolio on worksheets

--- components/app/student-cubby/PortfolioOverlay.vue
+++ components/app/student-cubby/PortfolioOverlay.vue
@@ -1,40 +1,42 @@
 <template>
-  <v-overlay :value="overlay" dark z-index="100">
-    <v-container class="pa-0 fullscreen" fluid>
-      <v-btn
-        class="top-left text-none white--text px-4"
-        text
-        x-large
-        @click.stop="overlay = false"
-      >
-        <v-icon left>
-          mdi-less-than
-        </v-icon>
-        Back
-      </v-btn>
-      <v-row class="fill-height" align="center" justify="center">
-        <v-col
-          class="position-relative"
-          cols="7"
-          sm="6"
-          md="5"
-          lg="4"
-          xl="3"
+  <v-dialog :value="overlay" fullscreen="true">
+    <div class="dialog-portfolio-overlay">
+      <v-container class="pa-0 fullscreen" fluid>
+        <v-btn
+          class="top-left text-none white--text px-4"
+          text
+          x-large
+          @click.stop="overlay = false"
         >
-          <v-btn
-            class="top-right"
-            icon
-            @click.stop="overlay = false"
+          <v-icon left>
+            mdi-less-than
+          </v-icon>
+          Back
+        </v-btn>
+        <v-row class="fill-height" align="center" justify="center">
+          <v-col
+            class="position-relative"
+            cols="7"
+            sm="6"
+            md="5"
+            lg="8"
+            xl="10"
           >
-            <v-icon>
-              mdi-close
-            </v-icon>
-          </v-btn>
-          <portfolio-card v-bind="{ image }" display-mode />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-overlay>
+            <v-btn
+              class="top-right color-close"
+              icon
+              @click.stop="overlay = false"
+            >
+              <v-icon>
+                mdi-close
+              </v-icon>
+            </v-btn>
+            <portfolio-card v-bind="{ image }" display-mode />
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </v-dialog>
 </template>
 
 <script>
@@ -76,7 +78,16 @@ export default {
 }
 
 .fullscreen {
-  width: 100vw !important;
-  height: 100vh !important;
+  width: 100% !important;
+  height: 100% !important;
+}
+
+.color-close {
+  color: white !important;
+}
+
+.dialog-portfolio-overlay {
+  background-color: rgba(0, 0, 0, 0.68) !important;
+  background: #1d1c1ccc !important;
 }
 </style>
