376e051a0aabd61efc6241d9e82a987347404cb5
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-09-25 19:00:00 -0500

    [FIX] fixes issue with menu styles

--- pages/admin/curriculum-management/_lessonId/preview.vue
+++ pages/admin/curriculum-management/_lessonId/preview.vue
@@ -37,7 +37,7 @@ export default {
     this.getLessonPreview(this.lessonId).then((lesson) => {
       this.lesson = lesson
       this.loading = false
-      if (this.$route.name === 'admin-curriculum-management-lessonId-preivew') {
+      if (this.$route.name === 'admin-curriculum-management-lessonId-preview') {
         this.$router.push(this.generateNuxtRoute('lesson-videos', { id: lesson.videos[0].id }))
       }
     })
