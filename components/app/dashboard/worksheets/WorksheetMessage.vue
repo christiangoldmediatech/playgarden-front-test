84481394395621ad0b6e524bdfba0f80135b6d13
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-10-02 09:47:16 -0500

    [ADD] changes to worksheet message

--- components/app/dashboard/worksheets/WorksheetMessage.vue
+++ components/app/dashboard/worksheets/WorksheetMessage.vue
@@ -1,45 +1,45 @@
 <template>
   <v-dialog
     :value="value"
-    max-width="640"
+    max-width="978px"
+    max-height="710px"
     persistent
     @input="$emit('input', $event.target.value)"
   >
     <v-card>
-      <v-card-title>
+      <div class="worksheet-message-content">
         <v-row>
-          <v-col cols="12">
+          <v-col class="pt-0" cols="12">
             <div class="worksheet-message-title">
               {{ `${correct ? 'GREAT JOB!' : 'TRY AGAIN!'}` }}
             </div>
           </v-col>
         </v-row>
-      </v-card-title>
 
-      <v-card-text>
         <v-row v-if="item" align="center" justify="center">
-          <v-col class="flex-shrink-1 flex-grow-0" cols="7" sm="5">
-            <div class="text-center worksheet-item">
+          <v-col cols="6" class="text-right">
+            <img
+              :src="item.image"
+              width="75%"
+            >
+            <!-- <div class="text-center worksheet-message-item">
               <v-img
                 class="mx-auto worksheet-item-shadow"
                 :src="item.image"
-                max-width="200"
                 aspect-ratio="1"
               >
                 <div class="worksheet-item-image worksheet-item-image-selected" />
               </v-img>
-              <div class="my-3 text-center font-weight-bold">
-                {{ item.word }}
-              </div>
-            </div>
+            </div> -->
           </v-col>
-          <v-col cols="5" sm="4">
+
+          <v-col cols="6">
             <div class="text-center">
               <v-img
                 :src="require(`@/assets/svg/${correct ? 'correct' : 'incorrect' }.svg`)"
                 class="mx-auto"
                 :aspect-ratio="1"
-                :max-width="100"
+                max-width="168px"
               />
               <div class="mt-2 text-h6 font-weight-bold">
                 {{ item.word }}
@@ -57,13 +57,16 @@
               </p>
             </div>
           </v-col>
-          <v-col cols="12" sm="9">
+        </v-row>
+
+        <v-row justify="center">
+          <v-col class="pb-0" cols="12" sm="9">
             <v-btn color="primary" block v-on="{ click: $listeners.click }">
               <slot />
             </v-btn>
           </v-col>
         </v-row>
-      </v-card-text>
+      </div>
     </v-card>
   </v-dialog>
 </template>
@@ -124,13 +127,24 @@ export default {
 </script>
 
 <style lang="scss">
-.worksheet {
-  &-message-title {
+.worksheet-message {
+  &-content {
+    padding: 45px 55px;
+  }
+  @media screen and (max-width: 599px) {
+    &-content {
+      padding: 12px 16px;
+    }
+  }
+  &-title {
     font-size: 28px !important;
     font-weight: bold !important;
     letter-spacing: 0.15em !important;
     text-align: center !important;
     display: block;
+  }
+  &-item {
+    max-width: 75%;
   }
 }
 </style>
