72a83432dce928afb1d30f473d9c80d4a4f54602
Jesse I. Angell <jesse.angell.1990@gmail.com>
2020-10-12 01:48:54 -0500

    [ADD] replaces all admin data tables with functional wrapper component

--- components/admin/curriculum/CurriculumTypesDataTable.vue
+++ components/admin/curriculum/CurriculumTypesDataTable.vue
@@ -40,110 +40,19 @@
       <v-col cols="12">
         <v-card width="100%">
           <v-card-text>
-            <v-data-table
+            <curriculum-types-editor-dialog ref="editor" />
+
+            <pg-admin-data-table
               :headers="headers"
-              hide-default-footer
               :items="types"
               :loading="loading"
               :page.sync="page"
               @update:page="page = $event"
-            >
-              <template v-slot:top>
-                <curriculum-types-editor-dialog ref="editor" />
-                <v-toolbar color="white" flat>
-                  <v-spacer />
-                  <pg-text-field
-                    v-model="search"
-                    append-icon="mdi-magnify"
-                    class="shrink"
-                    clearable
-                    hide-details
-                    label="Search"
-                    single-line
-                    solo
-                    @keydown.enter="refresh(false)"
-                  />
-                </v-toolbar>
-              </template>
-
-              <template v-slot:item.createdAt="{ item }">
-                {{ item.createdAt | formatDate }}
-              </template>
-
-              <template v-slot:item.updatedAt="{ item }">
-                {{ item.updatedAt | formatDate }}
-              </template>
-
-              <template v-slot:item.actions="{ item }">
-                <v-icon color="#81A1F7" dense @click="$refs.editor.open(item)">
-                  mdi-pencil-outline
-                </v-icon>
-                <v-icon color="#d30909" dense @click="remove(item)">
-                  mdi-delete-outline
-                </v-icon>
-              </template>
-
-              <template v-slot:no-data>
-                <v-btn color="primary" text @click="refresh(true)">
-                  Refresh
-                </v-btn>
-              </template>
-
-              <template v-slot:loading>
-                <v-skeleton-loader class="mx-auto" type="table-row-divider@3" />
-              </template>
-
-              <template v-slot:footer="{ props }">
-                <v-container fluid>
-                  <v-row align="center" justify="end">
-                    <v-icon
-                      class="clickable mr-2"
-                      color="green"
-                      :disabled="props.pagination.page === 1 || loading"
-                      x-small
-                      @click.stop="page--"
-                      v-text="'mdi-less-than'"
-                    />
-
-                    <template v-for="i in props.pagination.pageCount">
-                      <span
-                        :key="`footer-page-number-${i}`"
-                        :class="[
-                          'font-weight-normal',
-                          {
-                            'accent--text text--darken-1':
-                              props.pagination.page === i,
-                            clickable: props.pagination.page !== i
-                          }
-                        ]"
-                        @click.stop="page = i"
-                      >
-                        {{ i }}
-                      </span>
-                      <span
-                        v-if="i !== props.pagination.pageCount"
-                        :key="`footer-page-dot-${i}`"
-                        class="font-weight-normal mx-1"
-                      >
-                        &centerdot;
-                      </span>
-                    </template>
-
-                    <v-icon
-                      class="clickable ml-2"
-                      color="green"
-                      :disabled="
-                        props.pagination.page === props.pagination.pageCount ||
-                          loading
-                      "
-                      x-small
-                      @click.stop="page++"
-                      v-text="'mdi-greater-than'"
-                    />
-                  </v-row>
-                </v-container>
-              </template>
-            </v-data-table>
+              @refresh="refresh(true)"
+              @search="search = $event; refresh(false)"
+              @edit-item="$refs.editor.open($event)"
+              @remove-item="remove"
+            />
           </v-card-text>
         </v-card>
       </v-col>
---
+++ components/pg/components/data-table/PgAdminDataTable.vue
@@ -0,0 +1,180 @@
+<template functional>
+  <component
+    :is="$options.components.VDataTable"
+    dense
+    hide-default-footer
+    v-bind="props"
+    v-on="listeners"
+  >
+    <!-- Default Top Bar -->
+    <template v-if="$options.slotAvailable('top', scopedSlots)" v-slot:top>
+      <component :is="$options.components.VRow" :align="props.topAlign || 'start'" :justify="props.topJustify || 'end'">
+        <slot name="top.prepend" />
+
+        <component :is="$options.components.VCol" cols="12" md="4">
+          <component
+            :is="$options.components.PgTextField"
+            append-icon="mdi-magnify"
+            class="shrink"
+            clearable
+            hide-details
+            label="Search"
+            single-line
+            solo
+            @keydown.enter="$options.doEvent('search', $event.target.value, listeners)"
+          />
+        </component>
+      </component>
+    </template>
+
+    <!-- No Data -->
+    <template v-if="$options.slotAvailable('no-data', scopedSlots)" v-slot:no-data>
+      <component
+        :is="$options.components.VBtn"
+        color="primary"
+        text
+        @click="$options.doEvent('refresh', undefined, listeners)"
+      >
+        Refresh
+      </component>
+    </template>
+
+    <!-- Loading -->
+    <template v-if="$options.slotAvailable('loading', scopedSlots)" v-slot:loading>
+      <component :is="$options.components.VSkeletonLoader" class="mx-auto" type="table-row-divider@3" />
+    </template>
+
+    <!-- Created At -->
+    <template v-if="$options.slotAvailable('item.createdAt', scopedSlots)" v-slot:[`item.createdAt`]="{ item }">
+      {{ item.createdAt | formatDate }}
+    </template>
+
+    <!-- Updated At -->
+    <template v-if="$options.slotAvailable('item.updatedAt', scopedSlots)" v-slot:[`item.updatedAt`]="{ item }">
+      {{ item.updatedAt | formatDate }}
+    </template>
+
+    <!-- Item Actions -->
+    <template v-if="$options.slotAvailable('item.actions', scopedSlots)" v-slot:[`item.actions`]="{ item }">
+      <slot name="item.actions.prepend" v-bind="{ item }" />
+
+      <component
+        :is="$options.components.VIcon"
+        color="#81A1F7"
+        dense
+        @click.stop="$options.doEvent('edit-item', item, listeners)"
+      >
+        mdi-pencil-outline
+      </component>
+
+      <component
+        :is="$options.components.VIcon"
+        color="#d30909"
+        dense
+        @click.stop="$options.doEvent('remove-item', item, listeners)"
+      >
+        mdi-delete-outline
+      </component>
+
+      <slot name="item.actions.append" :v-bind="{ item }" />
+    </template>
+
+    <!-- Footer -->
+    <template v-if="$options.slotAvailable('footer', scopedSlots)" v-slot:footer="table">
+      <component :is="$options.components.VContainer" fluid>
+        <component :is="$options.components.VRow" align="center" justify="end">
+          <component
+            :is="$options.components.VIcon"
+            class="clickable mr-2"
+            color="green"
+            :disabled="table.props.pagination.page === 1 || props.loading"
+            x-small
+            @click.stop="$options.doEvent('update:page', table.props.pagination.page - 1, listeners)"
+            v-text="'mdi-less-than'"
+          />
+          <template v-for="i in table.props.pagination.pageCount">
+            <span
+              :key="`footer-page-number-${i}`"
+              :class="[
+                'font-weight-normal',
+                {
+                  'accent--text text--darken-1': table.props.pagination.page === i,
+                  'clickable': table.props.pagination.page !== i
+                }
+              ]"
+              @click.stop="$options.doEvent('update:page', table.props.pagination.page = i, listeners)"
+            >
+              {{ i }}
+            </span>
+            <span
+              v-if="i !== table.props.pagination.pageCount"
+              :key="`footer-page-dot-${i}`"
+              class="font-weight-normal mx-1"
+            >
+              &centerdot;
+            </span>
+          </template>
+
+          <component
+            :is="$options.components.VIcon"
+            class="clickable ml-2"
+            color="green"
+            :disabled="table.props.pagination.page === table.props.pagination.pageCount || props.loading"
+            x-small
+            @click.stop="$options.doEvent('update:page', table.props.pagination.page + 1, listeners)"
+            v-text="'mdi-greater-than'"
+          />
+        </component>
+      </component>
+    </template>
+
+    <!-- User defined slots -->
+    <template v-for="(index, name) in scopedSlots" v-slot:[name]="data">
+      <slot :name="name" v-bind="data" />
+    </template>
+  </component>
+</template>
+
+<script>
+import { VDataTable } from 'vuetify/lib/components/VDataTable'
+import { VContainer, VRow, VCol, VSpacer } from 'vuetify/lib/components/VGrid'
+import { VIcon } from 'vuetify/lib/components/VIcon'
+import { VSkeletonLoader } from 'vuetify/lib/components/VSkeletonLoader'
+import { VBtn } from 'vuetify/lib/components/VBtn'
+import PgTextField from '@/components/pg/components/form-inputs-controls/PgTextField.vue'
+
+export default {
+  name: 'PgAdminDataTable',
+
+  components: {
+    VDataTable,
+    VContainer,
+    VRow,
+    VCol,
+    VSpacer,
+    VIcon,
+    VSkeletonLoader,
+    VBtn,
+    PgTextField
+  },
+
+  slotAvailable (slot, scopedSlots) {
+    const keys = Object.keys(scopedSlots)
+    if (keys.includes(slot)) {
+      return false
+    }
+    return true
+  },
+
+  doEvent (name, value, listeners) {
+    if (listeners[name]) {
+      if (typeof listeners[name] === 'function') {
+        return listeners[name](value)
+      } else {
+        return listeners[name].map(callback => callback(value))
+      }
+    }
+    return undefined
+  }
+}
+</script>
