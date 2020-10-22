4ed51981846ab69b32b944cc246497e037f87c8b
Paola Boada <paolaboadaramos@gmail.com>
2020-10-22 13:48:53 -0300

    [ADD] dynamic puzzle

--- pages/app/student-cubby/puzzle.vue
+++ pages/app/student-cubby/puzzle.vue
@@ -10,15 +10,36 @@
         Complete each letter in the curriculum to unlock a new puzzle piece!
       </p>
 
-      <v-row justify="center">
-        <v-col cols="12">
-          <puzzle-cover
-            :background-image="
-              require('@/assets/jpg/student-cubby/puzzle-background.jpg')
-            "
-            :student-id="studentId"
-          />
+      <v-row>
+        <v-col>
+          <input v-model="seed" type="number" step="10" min="0" max="1000">
+          <br>
+          <input v-model="rows" type="number" min="1">
+          <input v-model="columns" type="number" min="1">
+          <br>
+          <input v-model="height" type="number" step="10" min="10">
+          <input v-model="width" type="number" step="10" min="10">
+          <br>
+          <input v-model="strokeWidth" type="number" step="0.5" min="0">
+          <br>
+          <v-color-picker v-model="strokeColor" show-swatches />
         </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <puzzle-cover
+          :background-image="
+            require('@/assets/jpg/student-cubby/puzzle-background.jpg')
+          "
+          :columns="columns"
+          :edges-seed="seed"
+          :height="height"
+          :rows="rows"
+          :student-id="studentId"
+          :stroke-color="strokeColor"
+          :stroke-width="strokeWidth"
+          :width="width"
+        />
       </v-row>
     </v-card-text>
   </v-card>
@@ -33,6 +54,16 @@ export default {
   components: {
     PuzzleCover
   },
+
+  data: () => ({
+    strokeColor: 'blue',
+    strokeWidth: 0.5,
+    width: 160,
+    height: 90,
+    rows: 4,
+    columns: 7,
+    seed: 0
+  }),
 
   computed: {
     studentId () {
