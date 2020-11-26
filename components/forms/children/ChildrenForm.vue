65bb7ad74a64bb6d98857db38e33b13a561e1a24
Christian Borja <>
2020-11-25 19:24:03 -0500

    add changes

---
+++ components/forms/children/ChildrenForm.vue
@@ -0,0 +1,269 @@
+<template>
+  <div class="">
+    <!-- Name -->
+    <validation-provider
+      v-slot="{ errors }"
+      :name="(removable ? `Child #${index + 1} - ` : '') + 'Name'"
+      rules="required"
+    >
+      <pg-text-field
+        v-model="item.firstName"
+        clearable
+        :disabled="isLoading"
+        :error-messages="errors"
+        label="Name"
+        solo
+        class="custom-text-field"
+      />
+    </validation-provider>
+    <!-- Birth date -->
+    <v-menu
+      ref="menu"
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <pg-text-field
+          label="Birth date"
+          readonly
+          solo
+          :suffix="item._birthdayFormatted ? '' : 'MM/DD/YYYY'"
+          validate-on-blur
+          :value="selectedDate"
+          v-bind="attrs"
+          class="custom-text-field"
+          v-on="on"
+        />
+      </template>
+      <v-date-picker
+        ref="picker"
+        v-model="item._birthdayPicker"
+        :max="new Date().toISOString().substr(0, 10)"
+        min="1950-01-01"
+        @input="onInputBirthday(item)"
+        @change="save"
+      />
+    </v-menu>
+
+    <!-- Gender -->
+    <validation-provider
+      :name="(removable ? `Child #${index + 1} - ` : '') + 'Gender'"
+      rules="required"
+    >
+      <v-row class="mb-6">
+        <v-col v-for="(gender, indexG) in genders" :key="indexG" cols="6">
+          <v-btn
+            block
+            class="custom-btn"
+            :color="item.gender === gender ? 'primary' : 'grey lighten-5'"
+            :disabled="isLoading"
+            min-height="60"
+            x-large
+            @click="item.gender = gender"
+          >
+            {{ gender === "FEMALE" ? "Girl" : "Boy" }}
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <input v-model="item.gender" type="hidden">
+    </validation-provider>
+
+    <!-- Backpack -->
+    <validation-provider
+      :name="(removable ? `Child #${index + 1} - ` : '') + 'Backpack'"
+      rules="required"
+    >
+      <v-row no-gutters>
+        <v-col
+          v-for="(backpack, indexB) in backpacks"
+          :key="indexB"
+          class="image"
+          cols="4"
+          md="2"
+        >
+          <img
+            :alt="backpack.name"
+            class="clickable"
+            :class="{ active: item.backpackId === backpack.id }"
+            :src="backpack.image"
+            height="100px"
+            @click="item.backpackId = backpack.id"
+          >
+        </v-col>
+      </v-row>
+
+      <input v-model="item.backpackId" type="hidden">
+    </validation-provider>
+
+    <v-btn
+      v-if="removable"
+      block
+      text
+      color="primary"
+      @click.stop="removeChild(item, index)"
+    >
+      DELETE CHILD PROFILE
+    </v-btn>
+
+    <v-divider v-if="removable" class="mt-6" />
+  </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'ChildrenForm',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    },
+    draft: {
+      type: Array,
+      required: true
+    },
+    loading: Boolean
+  },
+  data: () => ({
+    backpacks: [],
+    menu: false,
+    dataLoading: false,
+    genders: ['MALE', 'FEMALE'],
+    selectedDate: null
+  }),
+  computed: {
+    ...mapGetters('auth', ['isUserLoggedIn']),
+
+    removable () {
+      return this.draft.length > 1
+    },
+
+    isLoading () {
+      return this.dataLoading || this.loading
+    }
+  },
+  watch: {
+    menu (val) {
+      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+    }
+  },
+  created () {
+    this.fetchBackpacks()
+    if (this.isUserLoggedIn) {
+      this.loadChildren()
+    } else {
+      this.addRow()
+    }
+  },
+  methods: {
+    ...mapActions('backpacks', ['getBackpacks']),
+
+    ...mapActions('children', {
+      getChildren: 'get',
+      deleteChild: 'delete'
+    }),
+
+    fetchBackpacks () {
+      this.getBackpacks().then(data => (this.backpacks = data))
+    },
+
+    onInputBirthday (item) {
+      if (item._birthdayPicker) {
+        this.selectedDate = item._birthdayPicker
+        item._birthdayFormatted = dayjs(item._birthdayPicker).format(
+          'MM/DD/YYYY'
+        )
+        item.birthday = `${item._birthdayPicker}T00:00:00.000`
+      }
+    },
+
+    removeChild (item, index) {
+      console.log('item--', item)
+      console.log('index--', index)
+      this.$nuxt.$emit('open-prompt', {
+        title: 'Delete child profile?',
+        message: `Are you sure you wish to delete '${item.firstName}'s' profile?`,
+        action: async () => {
+          this.dataLoading = true
+          try {
+            if (item.id) {
+              await this.deleteChild(item.id)
+            }
+
+            this.$delete(this.draft, index)
+          } catch (e) {
+          } finally {
+            this.dataLoading = false
+          }
+        }
+      })
+    },
+
+    async loadChildren () {
+      try {
+        this.dataLoading = true
+
+        const rows = await this.getChildren()
+
+        if (rows.length) {
+          rows.map(this.addRow)
+        } else {
+          this.addRow()
+        }
+      } catch (e) {
+      } finally {
+        this.dataLoading = false
+      }
+    },
+
+    save (date) {
+      this.$refs.menu.save(date)
+      this.$emit('selected-date-birthday', date)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.image {
+  height: 100px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  img {
+    max-height: 100px;
+    max-width: 100px;
+    width: 100%;
+
+    &.active {
+      background-color: var(--v-secondary-base);
+      border-radius: 50%;
+      padding: 5px;
+    }
+  }
+}
+.grey {
+  color: var(--v-black-base);
+}
+
+.custom-btn::v-deep.v-btn.grey {
+  background-color: #f2f2f2;
+  color: #9d9d9d !important;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  text-transform: capitalize !important;
+}
+.custom-btn::v-deep.v-btn {
+  text-transform: capitalize !important;
+}
+</style>
--- components/forms/children/RegisterForm.vue
+++ components/forms/children/RegisterForm.vue
@@ -8,125 +8,8 @@
           >
             CHILD INFORMATION
           </p>
-
-          <!-- Name -->
-          <validation-provider
-            v-slot="{ errors }"
-            :name="(removable ? `Child #${indexD + 1} - ` : '') + 'Name'"
-            rules="required"
-          >
-            <pg-text-field
-              v-model="item.firstName"
-              clearable
-              :disabled="isLoading"
-              :error-messages="errors"
-              label="Name"
-              solo
-              class="custom-text-field"
-            />
-          </validation-provider>
-
-          <!-- Birth date -->
-          <v-menu
-            v-model="item._menu"
-            :close-on-content-click="false"
-            min-width="290px"
-            transition="scale-transition"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <validation-provider
-                v-slot="{ errors }"
-                :name="
-                  (removable ? `Child #${indexD + 1} - ` : '') + 'Birth date'
-                "
-                rules="required"
-              >
-                <pg-text-field
-                  v-bind="attrs"
-                  :disabled="isLoading"
-                  :error-messages="errors"
-                  label="Birth date"
-                  readonly
-                  solo
-                  :suffix="item._birthdayFormatted ? '' : 'MM/DD/YYYY'"
-                  validate-on-blur
-                  :value="item._birthdayFormatted"
-                  class="custom-text-field"
-                  v-on="on"
-                />
-              </validation-provider>
-            </template>
-
-            <v-date-picker
-              v-model="item._birthdayPicker"
-              :max="new Date().toISOString().substr(0, 10)"
-              min="1990-01-01"
-              @input="onInputBirthday(item)"
-            />
-          </v-menu>
-
-          <!-- Gender -->
-          <validation-provider
-            :name="(removable ? `Child #${indexD + 1} - ` : '') + 'Gender'"
-            rules="required"
-          >
-            <v-row class="mb-6">
-              <v-col v-for="(gender, indexG) in genders" :key="indexG" cols="6">
-                <v-btn
-                  block
-                  class="custom-btn"
-                  :color="item.gender === gender ? 'primary' : 'grey lighten-5'"
-                  :disabled="isLoading"
-                  min-height="60"
-                  x-large
-                  @click="item.gender = gender"
-                >
-                  {{ gender === "FEMALE" ? "Girl" : "Boy" }}
-                </v-btn>
-              </v-col>
-            </v-row>
-
-            <input v-model="item.gender" type="hidden">
-          </validation-provider>
-
-          <!-- Backpack -->
-          <validation-provider
-            :name="(removable ? `Child #${indexD + 1} - ` : '') + 'Backpack'"
-            rules="required"
-          >
-            <v-row no-gutters>
-              <v-col
-                v-for="(backpack, indexB) in backpacks"
-                :key="indexB"
-                class="image"
-                cols="4"
-                md="2"
-              >
-                <img
-                  :alt="backpack.name"
-                  class="clickable"
-                  :class="{ active: item.backpackId === backpack.id }"
-                  :src="backpack.image"
-                  height="100px"
-                  @click="item.backpackId = backpack.id"
-                >
-              </v-col>
-            </v-row>
-
-            <input v-model="item.backpackId" type="hidden">
-          </validation-provider>
-
-          <v-btn
-            v-if="removable"
-            block
-            text
-            color="primary"
-            @click.stop="removeChild(item, indexD)"
-          >
-            DELETE CHILD PROFILE
-          </v-btn>
-
-          <v-divider v-if="removable" class="mt-6" />
+          {{ indexD }}
+          <children-form :date="item._birthdayPicker" :item="item" :index="indexD" :draft="draft" :loading="loading" />
         </v-col>
       </v-row>
 
@@ -177,9 +60,14 @@ import { get } from 'lodash'
 import { mapActions, mapGetters } from 'vuex'
 
 import { jsonCopy } from '@/utils/objectTools'
+import ChildrenForm from '@/components/forms/children/ChildrenForm.vue'
 
 export default {
   name: 'RegisterForm',
+
+  components: {
+    ChildrenForm
+  },
 
   props: {
     loading: Boolean
@@ -189,7 +77,9 @@ export default {
     backpacks: [],
     draft: [],
     genders: ['MALE', 'FEMALE'],
-    dataLoading: false
+    dataLoading: false,
+    menuTest: false,
+    date: null
   }),
 
   computed: {
@@ -204,9 +94,13 @@ export default {
     }
   },
 
-  created () {
-    this.fetchBackpacks()
+  watch: {
+    menuTest (val) {
+      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+    }
+  },
 
+  created () {
     if (this.isUserLoggedIn) {
       this.loadChildren()
     } else {
@@ -215,7 +109,6 @@ export default {
   },
 
   methods: {
-    ...mapActions('backpacks', ['getBackpacks']),
 
     ...mapActions('children', {
       getChildren: 'get',
@@ -241,19 +134,6 @@ export default {
         lastName: child.lastName || ' ',
         level: child.level || 'BEGINNER'
       })
-    },
-
-    fetchBackpacks () {
-      this.getBackpacks().then(data => (this.backpacks = data))
-    },
-
-    onInputBirthday (item) {
-      if (item._birthdayPicker) {
-        item._birthdayFormatted = dayjs(item._birthdayPicker).format(
-          'MM/DD/YYYY'
-        )
-        item.birthday = `${item._birthdayPicker}T00:00:00.000`
-      }
     },
 
     onSubmit () {
@@ -300,38 +180,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.image {
-  height: 100px;
-  width: 100px;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-
-  img {
-    max-height: 100px;
-    max-width: 100px;
-    width: 100%;
-
-    &.active {
-      background-color: var(--v-secondary-base);
-      border-radius: 50%;
-      padding: 5px;
-    }
-  }
-}
-.grey {
-  color: var(--v-black-base);
-}
-
-.custom-btn::v-deep.v-btn.grey {
-  background-color: #f2f2f2;
-  color: #9d9d9d !important;
-  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-  text-transform: capitalize !important;
-}
-.custom-btn::v-deep.v-btn {
-  text-transform: capitalize !important;
-}
-</style>
