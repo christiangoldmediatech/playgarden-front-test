506fbddf811a12e7785123b0dc532525c0010abe
Christian Borja <>
2020-12-15 11:52:30 -0500

    add text payment

--- pages/app/payment/register.vue
+++ pages/app/payment/register.vue
@@ -25,11 +25,21 @@
         justify="center"
         no-gutters
       >
-        <v-col class="px-12" cols="12" lg="6" md="8" xl="6">
+        <v-col cols="12" class="px-12">
+          <p>
+            <span class="font-weight-bold text-h5">
+              Why do we need a credit card?
+            </span>
+          </p>
+          <p>
+            Playgarden Prep Online is currently <span class="font-weight-bold">FREE</span> for you for the next 30 days. We need your credit card information to complete the registration process, but you will <span class="font-weight-bold">NOT</span> be charged.
+          </p>
+        </v-col>
+        <v-col class="px-12 mt-12" cols="12" lg="6" md="8" xl="6">
           <stripe-form :loading="loading" @click:submit="onSubmit" />
         </v-col>
 
-        <v-col class="px-12" cols="12" lg="6" md="4" xl="6">
+        <v-col class="px-12 mt-12" cols="12" lg="6" md="4" xl="6">
           <p class="text-center text-md-left">
             <span class="font-weight-bold text-h5 pg-letter-spacing">
               MEMBERSHIP REVIEW
