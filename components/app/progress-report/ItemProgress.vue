87ec3fbe4c88db44390421ee5686190ffab24e24
Christian Borja <christian@goldmediatech.com>
2020-12-24 01:30:59 -0500

    add

---
+++ components/app/progress-report/ItemProgress.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row>
+    <v-col cols="2" class="ml-4">
+      <v-progress-circular
+        :rotate="360"
+        :size="100"
+        :width="15"
+        :value="item.progress"
+        color="accent"
+      >
+        {{ item.progress }}
+      </v-progress-circular>
+    </v-col>
+    <v-col class="ml-12">
+      <p class="ml-3">
+        <span>
+          {{ item.name }}
+        </span>
+      </p>
+      <p class="ml-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam et, posuere risus.
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ItemProgress',
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+
+  data: () => ({}),
+
+  computed: {},
+
+  watch: {},
+
+  created () {},
+
+  methods: {}
+}
+</script>
--- components/app/progress-report/LetterStats.vue
+++ components/app/progress-report/LetterStats.vue
@@ -1,33 +1,27 @@
 <template>
-  <v-list three-line>
-    <template v-for="(item, index) in types">
-      <v-list-item
-        :key="index"
-        class="mt-6 mb-6"
-      >
-        <v-list-item-avatar>
-          icon gauge
-        </v-list-item-avatar>
-
-        <v-list-item-content class="font-weight-bold ">
-          <span>
-            {{ item.name }}
-          </span>
-        </v-list-item-content>
-      </v-list-item>
-    </template>
-  </v-list>
+  <div class="">
+    <v-list three-line>
+      <template v-for="(item, index) in types">
+        <item-progress :key="index" :item="item" />
+      </template>
+    </v-list>
+  </div>
 </template>
 
 <script>
-
+import ItemProgress from './ItemProgress.vue'
 export default {
   name: 'LetterStats',
+
+  components: {
+    ItemProgress
+  },
 
   data: () => ({
     types: [
       {
-        name: 'test'
+        name: 'Daily Lessons Attended',
+        progress: 69
       }
     ]
   }),
