33987fba45f7a66bebc3205baf618e3dee5f18bd
Christian Borja <christian@goldmediatech.com>
2020-12-30 18:01:33 -0500

    change template

--- pages/app/progress-report.vue
+++ pages/app/progress-report.vue
@@ -2,22 +2,35 @@
   <v-container
     fluid
   >
-    <v-row no-gutters class="mt-4">
+    <v-row class="mt-4">
       <v-col cols="12" md="2" lg="2" xl="1">
-        <v-list three-line class="mt-9">
-          <v-list-item @click="general = true">
-            <v-list-item-avatar size="60">
-              <v-img :src="require('@/assets/svg/general.svg')" />
-            </v-list-item-avatar>
+        <v-card class="content-report">
+          <v-list three-line>
+            <v-list-item-group>
+              <template v-for="(item, index) in getMenu">
+                <v-list-item
+                  :key="index"
+                  class="mt-6 mb-6"
+                  active-class="active-css"
+                  @click="loadDetailReport(item.name)"
+                >
+                  <v-list-item-avatar size="60">
+                    <v-img v-if="item.name === 'General'" :src="require('@/assets/svg/general.svg')" />
+                    <v-img v-else :src="item.icon" min-width="38px" />
+                  </v-list-item-avatar>
 
-            <v-list-item-content>
-              <div class="">
-                <underlined-title class="text-h7 text-md-h6" text="General" />
-              </div>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+                  <v-list-item-content class="report-card-type">
+                    <span>
+                      {{ item.name }}
+                    </span>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
       </v-col>
+
       <v-col cols="12" md="10" lg="10" xl="11" class="pt-7">
         <v-card-text>
           <v-row no-gutters>
@@ -35,60 +48,33 @@
             </v-col>
           </v-row>
         </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="2" lg="2" xl="1">
-        <v-card class="content-report">
-          <v-list three-line>
-            <v-list-item-group>
-              <template v-for="(item, index) in types">
-                <v-list-item
-                  :key="index"
-                  class="mt-6 mb-6"
-                  active-class="active-css"
-                  @click="loadDetailReport(item.name)"
-                >
-                  <v-list-item-avatar size="60">
-                    <v-img :src="item.icon" min-width="38px" />
-                  </v-list-item-avatar>
-
-                  <v-list-item-content class="report-card-type">
-                    <span>
-                      {{ item.name }}
-                    </span>
-                  </v-list-item-content>
-                </v-list-item>
-              </template>
-            </v-list-item-group>
-          </v-list>
-        </v-card>
-      </v-col>
-
-      <v-col v-if="general === true" cols="12" md="10" lg="10" xl="11">
-        <v-card>
-          <v-row class="ml-2 mr-2">
-            <v-col cols="12" md="7" lg="7" xl="10">
-              <div class="pt-4 mb-4">
-                <underlined-title class="text-h6 text-md-h5" text="General Progress Report" />
-              </div>
-              <chart-report v-if="report" :report="report" />
-            </v-col>
-            <v-col cols="12" md="5" lg="5" xl="2">
-              <div class="pt-4 mb-4">
-                <underlined-title class="text-h6 text-md-h5" :text="letterStats.name" />
-              </div>
-              <v-card>
-                <letter-stats :letter-stats="letterStats" />
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-      <v-col v-else cols="12" md="10" lg="10" xl="11">
-        <v-card class="content-report mr-3">
-          <detail-progress :report-card-type="reportCardTypeSelected" :report="report" />
-        </v-card>
+        <v-row class="mr-1">
+          <v-col v-if="general === true" cols="12" md="12" lg="12" xl="12">
+            <v-card>
+              <v-row class="ml-2 mr-2">
+                <v-col cols="12" md="7" lg="7" xl="10">
+                  <div class="pt-4 mb-4">
+                    <underlined-title class="text-h6 text-md-h5" text="General Progress Report" />
+                  </div>
+                  <chart-report v-if="report" :report="report" />
+                </v-col>
+                <v-col cols="12" md="5" lg="5" xl="2">
+                  <div class="pt-4 mb-4">
+                    <underlined-title class="text-h6 text-md-h5" :text="letterStats.name" />
+                  </div>
+                  <v-card>
+                    <letter-stats :letter-stats="letterStats" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+          <v-col v-else cols="12" md="12" lg="12" xl="12">
+            <v-card class="content-report">
+              <detail-progress :report-card-type="reportCardTypeSelected" :report="report" />
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -131,6 +117,14 @@ export default {
 
     childrenIds () {
       return this.currentChild[0].id
+    },
+
+    getMenu () {
+      const menuGeneral = {
+        name: 'General',
+        icon: 'assets/svg/general.svg'
+      }
+      return [menuGeneral, ...this.types]
     },
 
     selectedChild: {
@@ -186,7 +180,11 @@ export default {
 
     loadDetailReport (reportCardType) {
       this.reportCardTypeSelected = reportCardType
-      this.general = false
+      if (reportCardType === 'General') {
+        this.general = true
+      } else {
+        this.general = false
+      }
     }
   }
 }
@@ -202,9 +200,7 @@ export default {
 }
 
 .active-css {
-  box-shadow: 0px 3px 9px #999 !important;
+  box-shadow: inset 2px 2px 11px 0px#D3D3D3 !important;
   color: white !important;
-  /* border-style: solid !important;
-  border-color: #ff0000 #00ff00 #0000ff #ffffff !important; */
 }
 </style>
