4a909b855503e9ee6715ee2cba83b033b35edd03
Christian Borja <christian@goldmediatech.com>
2020-12-30 19:19:24 -0500

    add change template

--- pages/app/progress-report.vue
+++ pages/app/progress-report.vue
@@ -48,9 +48,9 @@
             </v-col>
           </v-row>
         </v-card-text>
-        <v-row class="mr-1">
+        <v-row class="mr-1" no-gutters>
           <v-col v-if="general === true" cols="12" md="12" lg="12" xl="12">
-            <v-card>
+            <v-card class="content-report">
               <v-row class="ml-2 mr-2">
                 <v-col cols="12" md="7" lg="7" xl="10">
                   <div class="pt-4 mb-4">
