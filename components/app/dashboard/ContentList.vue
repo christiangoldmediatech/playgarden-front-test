a874eaa4233e1b9185273ada9efae301ae80bd66
Jesse I. Angell <jesse.angell.1990@gmail.com>
2021-01-19 01:58:49 -0600

    [ADD] adds lesson progress review to admin user profile

--- components/app/dashboard/ContentList.vue
+++ components/app/dashboard/ContentList.vue
@@ -1,15 +1,15 @@
 <template>
   <v-list class="py-0" dense>
     <v-list-item
-      v-for="item in items"
+      v-for="item in itemsComputed"
       :key="`list-${_uid}-item-${item.id}`"
       class="dashboard-item"
       active-class="dashboard-item-active"
       exact-active-class="dashboard-item-exact"
       :disabled="item.disabled"
-      nuxt
-      link
-      exact
+      :nuxt="item.nuxt"
+      :link="item.link"
+      :exact="item.exact"
       :to="item.to"
     >
       <v-list-item-avatar tile>
@@ -34,6 +34,11 @@
         <v-list-item-subtitle :class="{ 'dashboard-item-disabled': item.disabled }">
           <span :class="{ 'dashboard-item-disabled': item.disabled }">{{ item.description }}</span>
         </v-list-item-subtitle>
+        <v-list-item-subtitle v-if="noLinkMode" :class="{ 'dashboard-item-disabled': item.disabled }">
+          <span :class="{ 'dashboard-item-disabled': item.disabled }">
+            Position: {{ item.viewed | getTimeToMMSS }}
+          </span>
+        </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </v-list>
@@ -43,10 +48,53 @@
 export default {
   name: 'ContentList',
 
+  filters: {
+    getTimeToMMSS (viewed) {
+      if (viewed) {
+        const value = viewed.time
+        const measuredTime = new Date(null)
+        measuredTime.setSeconds(value)
+        const MHSTime = measuredTime.toISOString().substr(14, 5)
+        return MHSTime
+      }
+      return '00:00'
+    }
+  },
+
   props: {
     items: {
       type: Array,
       required: true
+    },
+
+    noLinkMode: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  computed: {
+    itemsComputed () {
+      let props = {
+        nuxt: true,
+        link: true,
+        exact: true
+      }
+      if (this.noLinkMode) {
+        props = {
+          nuxt: false,
+          link: false,
+          exact: false,
+          to: undefined
+        }
+      }
+      return this.items.map((item) => {
+        return {
+          ...item,
+          ...props
+        }
+      })
     }
   }
 }
