a7c132ec8190ddf47788458e92f20402c0b577f8
Christian Borja <christian@goldmediatech.com>
2021-02-25 16:55:45 -0500

    ready step one

---
+++ components/forms/auth/StepOneForm.vue
@@ -0,0 +1,273 @@
+<template>
+  <validation-observer v-slot="{ invalid, passes }">
+    <v-form @submit.prevent="passes(onSubmit)">
+      <v-container class="px-0">
+        <v-row no-gutters>
+          <v-col
+            :class="{ 'pr-2': $vuetify.breakpoint.mdAndUp }"
+            :cols="$vuetify.breakpoint.mdAndUp ? '6' : '12'"
+          >
+            <!-- First name -->
+            <validation-provider
+              v-slot="{ errors }"
+              name="Name"
+              rules="required"
+            >
+              <pg-text-field
+                v-model="draft.firstName"
+                clearable
+                :disabled="loading"
+                :error-messages="errors"
+                label="Name"
+                :loading="loading"
+                solo
+              />
+            </validation-provider>
+          </v-col>
+          <v-col
+            :class="{ 'pl-2': $vuetify.breakpoint.mdAndUp }"
+            :cols="$vuetify.breakpoint.mdAndUp ? '6' : '12'"
+          >
+            <!-- Last name -->
+            <validation-provider
+              v-slot="{ errors }"
+              name="Last Name"
+              rules="required"
+            >
+              <pg-text-field
+                v-model="draft.lastName"
+                clearable
+                :disabled="loading"
+                :error-messages="errors"
+                label="Last name"
+                :loading="loading"
+                solo
+              />
+            </validation-provider>
+          </v-col>
+          <v-col cols="12">
+            <v-row no-gutters>
+              <v-col cols="12">
+                <!-- Email -->
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Email"
+                  rules="required|email"
+                >
+                  <pg-text-field
+                    v-model="draft.email"
+                    clearable
+                    :disabled="
+                      loading || hasInvitationEmail || hasUserSocialData
+                    "
+                    :error-messages="errors"
+                    label="Email"
+                    :loading="loading"
+                    solo
+                    type="email"
+                  />
+                </validation-provider>
+
+                <template
+                  v-if="
+                    (inInvitationProcess || !isUserLoggedIn) && !userSocialData
+                  "
+                >
+                  <!-- Password -->
+                  <v-tooltip
+                    :bottom="$vuetify.breakpoint.xs"
+                    :right="$vuetify.breakpoint.smAndUp"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <validation-provider
+                        v-slot="{ errors }"
+                        name="Password"
+                        rules="required|min:6|max:20|w_number|w_special|w_upper"
+                        vid="password_field"
+                      >
+                        <pg-password-field
+                          v-model="draft.password"
+                          clearable
+                          :disabled="loading"
+                          :error-messages="errors"
+                          label="Password"
+                          :loading="loading"
+                          maxlength="20"
+                          solo
+                          v-bind="attrs"
+                          v-on="on"
+                        />
+                      </validation-provider>
+                    </template>
+
+                    <span class="white--text">
+                      At least 6 characters—the more characters, the better.
+                    </span>
+                    <br>
+                    <span class="white--text">
+                      A mixture of both uppercase and lowercase letters.
+                    </span>
+                    <br>
+                    <span class="white--text">
+                      A mixture of letters and numbers.
+                    </span>
+                    <br>
+                    <span class="white--text">
+                      Inclusion of at least one special character, e.g., !@#?]
+                    </span>
+                    <br>
+                  </v-tooltip>
+                </template>
+
+                <v-btn
+                  block
+                  min-height="60"
+                  class="mb-6 main-btn"
+                  color="primary"
+                  :disabled="invalid"
+                  :loading="loading"
+                  type="submit"
+                  x-large
+                >
+                  NEXT
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <!-- or -->
+            <v-row no-gutters class="my-6">
+              <v-col class="hr-line">
+                <v-divider />
+              </v-col>
+
+              <v-col class="text-center">
+                or
+              </v-col>
+
+              <v-col class="hr-line">
+                <v-divider />
+              </v-col>
+            </v-row>
+
+            <!-- Social buttons -->
+            <v-row no-gutters>
+              <!-- FACEBOOK -->
+              <v-col class="mb-4 mb-md-0 pr-md-4" cols="12" md="6">
+                <v-btn block height="45" class="social-btn" @click="facebookSignIn">
+                  <img
+                    alt="Facebook"
+                    class="mr-1"
+                    src="@/assets/svg/facebook_icon.svg"
+                  >
+
+                  <span class="spanSocialNetwork">Login with Facebook</span>
+                </v-btn>
+              </v-col>
+
+              <!-- GOOGLE -->
+              <v-col class="mb-6 mb-md-0 pl-md-4" cols="12" md="6">
+                <v-btn block height="45" class="social-btn" @click="googleSignIn">
+                  <img
+                    alt="Google"
+                    class="mr-1"
+                    src="@/assets/svg/google_icon.svg"
+                  >
+
+                  <span class="spanSocialNetwork">Login with Google</span>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </validation-observer>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+import { jsonCopy } from '@/utils/objectTools'
+
+export default {
+  name: 'StepOneForm',
+
+  props: {
+    emailValidated: {
+      type: String,
+      default: null
+    },
+
+    inInvitationProcess: Boolean,
+
+    loading: Boolean,
+
+    userSocialData: {
+      type: Object,
+      default: undefined
+    }
+  },
+
+  data: () => ({
+    draft: {},
+    show: true
+  }),
+
+  computed: {
+    ...mapGetters('auth', ['getUserInfo', 'isUserLoggedIn']),
+
+    hasInvitationEmail () {
+      return Boolean(
+        (this.inInvitationProcess && this.$route.query.email) ||
+          this.isUserLoggedIn
+      )
+    },
+
+    hasInvitationPhone () {
+      return Boolean(this.inInvitationProcess && this.$route.query.phone)
+    },
+
+    hasUserSocialData () {
+      return Boolean(this.userSocialData)
+    }
+  },
+
+  mounted () {
+    this.draft = {
+      firstName: this.hasUserSocialData
+        ? this.userSocialData.firstName
+        : this.getUserInfo.firstName || null,
+      lastName: this.hasUserSocialData
+        ? this.userSocialData.lastName
+        : this.getUserInfo.lastName || null,
+      phoneNumber:
+        this.$route.query.phone || this.getUserInfo.phoneNumber || null,
+      email: this.hasUserSocialData
+        ? this.userSocialData.email
+        : this.$route.query.email ||
+          this.getUserInfo.email ||
+          this.emailValidated ||
+          null,
+      password: null,
+      passwordConfirmation: null,
+      socialNetwork: this.hasUserSocialData
+        ? this.userSocialData.socialNetwork
+        : null,
+      socialNetworkId: this.hasUserSocialData
+        ? this.userSocialData.socialNetworkId
+        : null
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      this.$emit(
+        'click:submit',
+        jsonCopy({
+          ...this.draft
+        })
+      )
+    }
+  }
+}
+</script>
---
+++ pages/auth/signup-flow.vue
@@ -0,0 +1,294 @@
+<template>
+  <v-row
+    no-gutters
+  >
+    <v-col>
+      <v-row no-gutters>
+        <v-btn
+          color="accent"
+          nuxt
+          text
+          :to="{ name: 'auth-login' }"
+        >
+          <v-icon left>
+            mdi-less-than
+          </v-icon>
+
+          Back
+        </v-btn>
+      </v-row>
+
+      <v-row
+        class="flex-column-reverse flex-md-row"
+        justify="center"
+        no-gutters
+      >
+        <v-col class="px-12" cols="12" md="8">
+          <p class="text-center text-md-left">
+            <span class="font-weight-bold text-h5 pg-letter-spacing">
+              JOIN FOR FREE!
+            </span>
+          </p>
+
+          <step-one-form
+            :email-validated="emailValidated"
+            :in-invitation-process="inInvitationProcess"
+            :loading="loading"
+            :user-social-data="userSocialData"
+            @click:submit="onSubmit"
+          />
+        </v-col>
+
+        <v-col class="px-12" cols="12" md="4">
+          <p class="mt-6 mb-12 text-center text-md-left">
+            <small>
+              Complete the registration and choose the plan that best suits you, to start your learning experience!
+            </small>
+          </p>
+
+          <template v-if="!inInvitationProcess">
+            <p class="text-center text-md-left mt-6">
+              <v-row no-gutters>
+                <v-col>
+                  <img
+                    src="@/assets/png/gift-icon.png"
+                    class="clickable mr-2"
+                    width="18px"
+                  >
+
+                  <span
+                    class="font-weight-bold text-uppercase pg-letter-spacing-subtitle"
+                  >
+                    GET 30 DAYS OF FREE TRIAL
+                  </span>
+                  <v-row no-gutters>
+                    <span class="font-weight-bold mt-3">That includes:</span>
+                  </v-row>
+                  <v-row>
+                    <ul>
+                      <li
+                        class="register-item text-left"
+                      >
+                        <small class="text-trial">
+                          A Daily Learning Schedule
+                        </small>
+                      </li>
+                      <li
+                        class="register-item text-left"
+                      >
+                        <small class="text-trial">
+                          Access to over 1,200 lessons
+                        </small>
+                      </li>
+                      <li
+                        class="register-item text-left"
+                      >
+                        <small class="text-trial">
+                          Live Classes with Playgarden Prep Teachers
+                        </small>
+                      </li>
+                    </ul>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </p>
+
+            <p class="text-center text-md-left mt-10">
+              <small>*You can cancel your membership any time from the account
+                settings.</small>
+            </p>
+          </template>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+import StepOneForm from '@/components/forms/auth/StepOneForm'
+
+export default {
+  name: 'SignupFlow',
+
+  components: {
+    StepOneForm
+  },
+
+  data: vm => ({
+    loading: false,
+    emailValidated: null,
+
+    userSocialData: (() => {
+      const { query } = vm.$route
+      if (query.process === 'social-signup' && query._u) {
+        try {
+          return JSON.parse(atob(query._u))
+        } catch (e) {}
+      }
+
+      return null
+    })(),
+
+    token: vm.$route.query.token
+  }),
+
+  computed: {
+    ...mapGetters('auth', ['getUserInfo', 'isUserLoggedIn']),
+
+    inInvitationProcess () {
+      const { query } = this.$route
+
+      return Boolean(
+        (query.process === 'invitation-caregiver' ||
+          query.process === 'invitation-playdate') &&
+          (query.email || query.phone) &&
+          query.token
+      )
+    },
+
+    signupProcess () {
+      if (
+        this.inInvitationProcess &&
+        this.$route.query.process === 'invitation-caregiver'
+      ) {
+        return 'CAREGIVER'
+      }
+
+      return 'PARENT'
+    },
+
+    signupProcessCaregiver () {
+      return this.signupProcess === 'CAREGIVER'
+    }
+  },
+
+  beforeMount () {
+    if (this.userSocialData) {
+      this.emailValidated = this.userSocialData.email
+    }
+  },
+
+  methods: {
+    ...mapActions('auth/signup', {
+      newParent: 'signup',
+      validateEmail: 'signupEmail',
+      updateParentRegister: 'signupToken'
+    }),
+
+    ...mapActions('caregiver', { newCaregiver: 'signup' }),
+
+    ...mapActions('auth', ['setPlaydateInvitationToken']),
+
+    async onSubmit (data) {
+      try {
+        this.loading = true
+
+        await this.registerProcess(
+          this.signupProcessCaregiver ? { data, token: this.token } : data
+        )
+
+        if (this.signupProcessCaregiver) {
+          await this.$router.push({
+            name: 'app-dashboard'
+          })
+        } else if (this.isUserLoggedIn || this.inInvitationProcess) {
+          if (this.inInvitationProcess) {
+            this.setPlaydateInvitationToken(this.token)
+          }
+
+          await this.$router.push({
+            name: 'app-children-register',
+            query: { process: 'signup', step: '2' }
+          })
+        } else {
+          this.emailValidated = data.email
+        }
+
+        if (!this.emailValidated) {
+          this.$snotify.success('Welcome to Playgarden Prep!')
+        }
+      } catch (e) {
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async registerProcess (data) {
+      if (this.isUserLoggedIn) {
+        return await this.updateParentRegister(data)
+      }
+
+      if (this.emailValidated) {
+        return await this.newParent(data)
+      }
+
+      if (this.inInvitationProcess && this.signupProcessCaregiver) {
+        return await this.newCaregiver(data)
+      } else if (this.inInvitationProcess) {
+        return await this.newParent(data)
+      }
+
+      return await this.validateEmail(data)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.image {
+  max-height: 500px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  img {
+    max-width: 90%;
+  }
+  &.mobile {
+    max-height: 250px;
+  }
+}
+.form {
+  max-width: 500px;
+}
+.text-orange-info {
+  background-color: var(--v-accent-base) !important;
+  color: var(--v-white-base) !important;
+  height: 19px;
+  font-size: 15px;
+}
+
+.text-orange-info::v-deep.v-chip .v-chip__content {
+  color: var(--v-white-base) !important;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 3.15px;
+  line-height: 1.48;
+  background-color: var(--v-accent-base) !important;
+}
+
+.text-orange-info::v-deep.v-chip--label {
+  border-radius: 0px !important;
+}
+
+ul {
+  list-style: none; /* Remove default bullets */
+}
+
+ul li::before {
+  content: "●";
+  font-size: 28px;
+  color: var(--v-accent-base); /* Change the color */
+  font-weight: bold; /* If you want it to be bold */
+  display: inline-block; /* Needed to add space between the bullet and the text */
+  width: 0.7em; /* Also needed for space (tweak if needed) */
+  margin-left: -1rem; /* Also needed for space (tweak if needed) */
+}
+
+.text-trial {
+  margin-top: 11px;
+  position: absolute;
+}
+</style>
