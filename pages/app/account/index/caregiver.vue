2d150d4d45c1f9e4feec057688b37393f27dfddf
Esteban <esteban.lds@outlook.com>
2021-03-04 20:21:55 -0500

    added  caregiver page

---
+++ pages/app/account/index/caregiver.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <caregiver-list manageable />
+    <manage-caregivers />
+  </div>
+</template>
+
+<script>
+import CaregiverList from '@/components/app/caregiver/CaregiverList'
+import ManageCaregivers from '@/components/app/caregiver/ManageCaregivers'
+
+export default {
+  name: 'Caregiver',
+  components: {
+    CaregiverList,
+    ManageCaregivers
+  }
+}
+</script>
