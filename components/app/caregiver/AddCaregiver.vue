15497fb1b313f2fea2670e927b1ee6600ca61164
Esteban <esteban.lds@outlook.com>
2021-03-10 21:46:43 -0500

    created new component with only add caregiver functionality

---
+++ components/app/caregiver/AddCaregiver.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-dialog
+    v-model="modal"
+    content-class="white"
+    :fullscreen="isMobile"
+    max-width="600"
+    persistent
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        block
+        class="mb-6"
+        color="primary"
+        x-large
+        v-on="on"
+      >
+        Add Caregiver
+      </v-btn>
+    </template>
+
+    <v-col cols="12">
+      <v-row class="pr-3" justify="end">
+        <v-btn icon @click.stop="modal = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-row>
+
+      <v-row justify="center">
+        <div class="text-uppercase font-weight-bold text-h5 text-md-h4 grey--text text--darken-2">
+          Add Caregiver
+        </div>
+      </v-row>
+
+      <v-row v-if="modal" class="flex-column-reverse flex-md-row">
+        <v-col class="pt-5 px-6" cols="12">
+          <send-invitation-form
+            :loading="loading"
+            @click:cancel="onCancel"
+            @click:submit="onSubmit"
+          />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+import SendInvitationForm from '@/components/forms/caregiver/SendInvitationForm'
+
+export default {
+  name: 'AddCaregiver',
+
+  components: {
+    SendInvitationForm
+  },
+
+  data: () => ({
+    loading: false,
+    modal: false
+  }),
+
+  computed: {
+    isMobile () {
+      return this.$vuetify.breakpoint.mobile
+    }
+  },
+
+  methods: {
+    ...mapActions('caregiver', [
+      'fetchCaregiverInvitationList',
+      'sendCaregiverInvitation'
+    ]),
+
+    onCancel () {
+      this.loading = false
+      this.modal = false
+    },
+
+    async onSubmit (draft) {
+      this.loading = true
+
+      try {
+        await this.sendCaregiverInvitation(draft)
+        this.modal = false
+
+        this.$snotify.success('Invitation has been sent successfully!')
+      } catch (e) {
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
