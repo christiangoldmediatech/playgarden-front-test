f103d587fc89920fd954b9379c215433f6a0315d
Esteban <esteban.lds@outlook.com>
2021-03-10 21:46:29 -0500

    removed manage-caregivers from index

--- pages/app/account/index/caregiver.vue
+++ pages/app/account/index/caregiver.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <caregiver-list manageable />
-    <manage-caregivers />
   </div>
 </template>
 
 <script>
 import CaregiverList from '@/components/app/caregiver/CaregiverList'
-import ManageCaregivers from '@/components/app/caregiver/ManageCaregivers'
 
 export default {
   name: 'Caregiver',
   components: {
-    CaregiverList,
-    ManageCaregivers
+    CaregiverList
   }
 }
 </script>
