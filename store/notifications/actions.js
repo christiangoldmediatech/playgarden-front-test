a576b5abc4c77973f6b80ee60a8696641845a8be
Esteban <esteban.lds@outlook.com>
2021-03-17 13:44:45 -0500

    moved shipping verification to the store

--- store/notifications/actions.js
+++ store/notifications/actions.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { snotifyError } from '@/utils/vuex'
 
 export default {
@@ -26,5 +27,35 @@ export default {
 
   updateNotification (_, { id, data }) {
     return this.$axios.$patch(`/notifications/${id}`, data)
+  },
+
+  /**
+   * Show a notification prompting the user to update their shipping address if:
+   * - The user doesn't have the shipping address on file, and
+   * - 2 days or more have past.
+   */
+  async checkUserShippingAddressAndNotify ({ commit, dispatch, rootGetters }) {
+    const isUserLoggedIn = rootGetters['auth/isUserLoggedIn']
+    const userInfo = rootGetters['auth/getUserInfo']
+
+    if (isUserLoggedIn) {
+      try {
+        await dispatch('shipping-address/getShippingAddress', undefined, { root: true })
+      } catch (error) {
+        if (error?.response?.status === 404) {
+          const createdAt = userInfo.createdAt
+          const daysDifference = dayjs(new Date()).diff(dayjs(createdAt), 'days')
+
+          if (daysDifference >= 2) {
+            commit('notifications/SET_NOTIFICATION_CARD', {
+              title: 'WE WANT TO SEND YOU A WELCOME KIT!',
+              description: 'We require a shipping address in order to send the Welcome Kit with Backpack, workbooks, and additional materials.',
+              action: () => commit('notifications/SET_IS_SHIPPING_MODAL_VISIBLE', true, { root: true }),
+              image: require('@/assets/png/megaphone.png')
+            }, { root: true })
+          }
+        }
+      }
+    }
   }
 }
