6d30e1eb1f3441d7a845288c8896f6b02c49b8bb
Esteban <esteban.lds@outlook.com>
2021-03-22 09:29:38 -0500

    updated routeHandler

--- middleware/routeHandler.js
+++ middleware/routeHandler.js
@@ -3,16 +3,20 @@ import unauthenticatedRoutes from '~/utils/consts/unauthenticatedRoutes.json'
 import parentSubscriptionWhitelistedRoutes from '~/utils/consts/parentSubscriptionWhitelistedRoutes.json'
 import routeHandlerIgnoredRoutes from '~/utils/consts/routeHandlerIgnoredRoutes.json'
 
-export default function ({ redirect, route, store }) {
-  const user = store.getters['auth/getUserInfo']
-  const isLoggedIn = store.getters['auth/checkAuth']
-  const isAppRoute = /^app-.*$/.test(route.name)
-  const isAdminRoute = /^admin.*$/.test(route.name)
-  const isUnauthenticatedRoute = !!unauthenticatedRoutes[route.name]
+export default async function ({ app, redirect, route, store, req }) {
+  const isIgnoredRoute = !!routeHandlerIgnoredRoutes[route.name]
 
-  if (routeHandlerIgnoredRoutes[route.name]) {
+  if (isIgnoredRoute) {
     return
   }
+
+  const isLoggedIn = await store.dispatch('auth/checkAuth')
+  const isUnauthenticatedRoute = !!unauthenticatedRoutes[route.name]
+  const user = store.getters['auth/getUserInfo']
+
+  /**
+   * ROLE
+   */
 
   const shouldRedirectUser =
     isLoggedIn &&
@@ -24,23 +28,43 @@ export default function ({ redirect, route, store }) {
     return
   }
 
+  const isAppRoute = /^app-.*$/.test(route.name)
+  const isAdminRoute = /^admin.*$/.test(route.name)
   const isAdminRole = get(user, 'role.section') === 'ADMIN'
   const isUserRole = get(user, 'role.section') === 'USERS'
 
   if ((isUnauthenticatedRoute || isAppRoute) && isAdminRole) {
-    get(user, 'role.name') === 'SPECIALISTS'
+    return get(user, 'role.name') === 'SPECIALISTS'
       ? redirect({ name: 'admin-agenda' })
       : redirect({ name: 'admin-curriculum-management' })
   } else if ((isUnauthenticatedRoute || isAdminRoute) && isUserRole) {
-    redirect({ name: 'app-dashboard' })
+    return redirect({ name: 'app-dashboard' })
   }
+
+  /**
+   * CANCELED SUBSCRIPTION
+   */
 
   const shouldRedirectToAccount =
     !parentSubscriptionWhitelistedRoutes[route.name] &&
-    get(user, 'role.id') === 3 && // PARENT
+    get(user, 'role.id') === 3 /* PARENT */ &&
     (!user.subscription || user.subscription.status === 'canceled')
 
   if (shouldRedirectToAccount) {
-    redirect({ name: 'app-account-index' })
+    return redirect({ name: 'app-account-index' })
+  }
+
+  /**
+   * EMAIL VERIFIED
+   */
+
+  const shouldRedirectToDashboard =
+    user.id &&
+    user.validatedDate &&
+    route.name === 'auth-verify-email' &&
+    !['auth-logout', 'jwt-recovery'].includes(route.name)
+
+  if (shouldRedirectToDashboard) {
+    return redirect({ name: 'app=dashboard' })
   }
 }
