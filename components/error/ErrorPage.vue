c9867ada8ee0c0a87c6543ceff17688b338f6413
Esteban <estebi@goldmediatech.com>
2021-03-24 23:40:30 -0500

    added component with title and img

---
+++ components/error/ErrorPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-container fluid :class="{ 'fill-height': !isMobile, 'mt-12 mt-md-0': true }">
+    <v-row align="center" justify="center" no-gutters>
+      <v-col
+        cols="12"
+        md="5"
+        offset-md="1"
+        :order="isMobile ? 2 : 1"
+        class="text-center text-md-left"
+      >
+        <div>
+          <img src="@/assets/png/error-window.png" height="50px" class="mb-2 pr-3">
+          <img src="@/assets/svg/logo.svg" height="50px">
+        </div>
+        <underlined-title
+          :text="title"
+          font-size="48px"
+          font-size-mobile="32px"
+          letter-spacing="10px"
+        />
+        <div class="text-h5 text-md-h4 font-weight-medium grey--text text--darken-2 my-4">
+          {{ description }}
+        </div>
+        <v-btn
+          class="my-3"
+          color="warning"
+          large
+          router
+          :to="{ name: 'app-dashboard' }"
+        >
+          Go Back To Lessons
+        </v-btn>
+        <v-btn
+          class="my-3"
+          color="warning"
+          large
+          outlined
+          router
+          :to="{ name: 'app-library' }"
+        >
+          Go To Library
+        </v-btn>
+      </v-col>
+      <v-col cols="12" md="5" class="d-flex justify-left" :order="isMobile ? 1 : 2">
+        <v-img
+          :max-height="isMobile ? '400px' : '700px'"
+          contain
+          :src="img"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    img: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    isMobile () {
+      return this.$vuetify.breakpoint.mobile
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.fill-height {
+  height: 100vh !important;
+}
+.v-btn:not(.v-btn--text) {
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16) !important;
+}
+</style>
