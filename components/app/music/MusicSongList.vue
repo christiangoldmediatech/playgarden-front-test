298743af54ea253389fa7e7dcdac90743699fb81
uboat46 <uboat46@gmail.com>
2021-04-06 23:43:10 -0500

    [ADD] son list layout

---
+++ components/app/music/MusicSongList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="fill-height pa-4">
+    <v-row no-gutters>
+      <v-col cols="12" md="8">
+        <music-carousel-letter :value="null" />
+      </v-col>
+      <v-col cols="12" md="">
+        filter
+      </v-col>
+    </v-row>
+    <div>
+      MSUCISS
+    </div>
+  </div>
+</template>
+
+<script>
+import MusicCarouselLetter from '@/components/app/music/MusicLetterCarousel.vue'
+
+export default {
+  name: 'MusicSongList',
+  components: {
+    MusicCarouselLetter
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
