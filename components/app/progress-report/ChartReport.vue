5217c92c366b0c3f61c358dbe142c356f7f4c00b
Christian Borja <Christian@Borja>
2021-04-12 14:44:33 -0500

    add

--- components/app/progress-report/ChartReport.vue
+++ components/app/progress-report/ChartReport.vue
@@ -27,13 +27,6 @@ export default {
     getCategories () {
       return (this.report.categories) ? this.report.categories : []
     },
-    getSeries () {
-      return (this.report.dataSerie) ? {
-        symbolSize: this.getIconSize,
-        data: this.report.dataSerie,
-        type: 'scatter'
-      } : {}
-    },
     getRotate () {
       return (this.$vuetify.breakpoint.xs) ? 30 : 0
     },
@@ -88,7 +81,25 @@ export default {
         },
         series: [{
           symbolSize: 50,
-          data: this.report.dataSerie,
+          data: [
+            {
+              value: 70,
+              symbol: 'image://https://img.playgardenonline.com/images/report-card-icon/e145b9f9-22af-446b-bb48-a35ce1e2f902.png'
+            },
+            {
+              value: 20,
+              symbol: 'image://https://img.playgardenonline.com/images/report-card-icon/b083c2e6-0036-44b2-95f2-e19fa3c9b422.png'
+            },
+            {
+              value: 67,
+              ageAppropiate: 5,
+              symbol: 'image://https://img.playgardenonline.com/images/report-card-icon/4afe0455-bbdb-4791-8b92-5bf3d28bcb6e.png'
+            },
+            {
+              value: 16,
+              symbol: 'image://https://img.playgardenonline.com/images/report-card-icon/a91c42d5-589f-4dca-a44c-5a7fcd826bf7.png'
+            }
+          ], // this.report.dataSerie,
           type: 'scatter',
           markArea: {
             label: {
