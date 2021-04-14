ff77559b4298cdba0e02d8e2a2e85376046a23b5
Christian Borja <Christian@Borja>
2021-04-13 21:07:53 -0500

    add

--- store/auth/login/actions.js
+++ store/auth/login/actions.js
@@ -6,10 +6,23 @@ export default {
       const response = await this.$axios.post('/auth/login', data)
 
       dispatch('auth/setToken', response.data.accessToken, { root: true })
-    } catch (error) {
+    } catch (e) {
+      const { data } = e.response
+      console.log('data', data)
+      if (data && data.statusCode === 400) {
+        await this.$router.push({
+          name: 'app-inactive-subscription'
+        })
+      } else {
+        snotifyError(commit, {
+          body: 'Sorry! Wrong email or password.'
+        })
+      }
+    }
+    /* } catch (error) {
       snotifyError(commit, {
         body: 'Sorry! Wrong email or password.'
       })
-    }
+    } */
   }
 }
