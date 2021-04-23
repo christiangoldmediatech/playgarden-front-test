fbb9ac5859f6ba4b51285762ff2078b05c049ec8
Esteban <estebi@goldmediatech.com>
2021-04-22 23:25:12 -0500

    allow chart dimensions to be set by js

--- components/admin/dashboard/BarChart.vue
+++ components/admin/dashboard/BarChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <chart ref="bar-chart" :graph="getFormatGraph" :autoresize="resize" />
+  <chart ref="bar-chart" :style="getChartDimensions" :graph="getFormatGraph" :autoresize="resize" />
 </template>
 
 <script>
@@ -14,6 +14,14 @@ export default {
       type: Object,
       required: true,
       default: () => ({})
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
     }
   },
   data () {
@@ -41,18 +49,13 @@ export default {
         }],
         color: ['#F89838', '#925A22', '#fc8452', '#C2DAA5', '#78C383', '#9a60b4', '#ee6666', '#3ba272', '#ea7ccc']
       }
+    },
+    getChartDimensions () {
+      return {
+        height: `${this.height} !important`,
+        width: `${this.width} !important`
+      }
     }
   }
 }
 </script>
-
-<style>
-/**
- * The default size is 600px×400px, for responsive charts
- * you may need to set percentage values as follows (also
- * don't forget to provide a size for the container).
- */
-.echarts {
-  width: 100% !important;
-}
-</style>
