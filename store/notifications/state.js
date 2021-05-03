07ce9c9bcb8214c81c3cc79603f514cae0dc6d5b
Esteban <estebi@goldmediatech.com>
2021-05-03 01:33:23 -0500

    updated ribbon height values

--- store/notifications/state.js
+++ store/notifications/state.js
@@ -1,6 +1,6 @@
 export default () => ({
-  expiringRibbonHeightDesktop: 57, // update if ribbon content is modified
-  expiringRibbonHeightMobile: 104, // update if ribbon content is modified
+  expiringRibbonHeightDesktop: 54, // update if ribbon content is modified
+  expiringRibbonHeightMobile: 101, // update if ribbon content is modified
   isTrialExpiringRibbonVisible: false,
   isTrialExpiredModalVisible: false,
   isShippingModalVisible: false,
