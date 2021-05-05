51125c8c2b4d106b8f0a3a0310e5858049c59c4b
emiliano-sotomayor <emi@goldmediatech.com>
2021-05-05 15:36:50 -0500

    [FIX]

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -62,7 +62,8 @@ Cypress.Commands.add('headlessLoginAs', (user: 'parent' | 'admin') => {
 const tomorrow = Date.now() + 8.6e7
 
 Cypress.Commands.add('headlessChildrenFetch', () => {
-  const token = JSON.parse(window.localStorage.getItem('authToken'))
+  const tokenString = window.localStorage.getItem('authToken') || ''
+  const token = JSON.parse(tokenString)
 
   return cy.request({
     method: 'GET',
