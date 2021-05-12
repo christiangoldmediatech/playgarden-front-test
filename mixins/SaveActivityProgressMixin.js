201c27ba27fb7f8370726f4cb53b312f928934a7
Jesse I. Angell <jesse.angell.1990@gmail.com>
2021-05-12 11:47:21 -0500

    [FIX] removs if statement, now always sends video data

--- mixins/SaveActivityProgressMixin.js
+++ mixins/SaveActivityProgressMixin.js
@@ -27,38 +27,33 @@ export default {
       const promises = []
 
       // Only save progress if the video hasn't been completed and we are ahead of where we last left off
-      if (
-        !currentVideo.viewed ||
-        (!currentVideo.viewed.completed && currentVideo.viewed.time < time)
-      ) {
-        this.savingActivityProgress = true
-        this.children.forEach((child) => {
-          promises.push(
-            this.sendActivityProgress({
-              lessonId: this.lesson.id,
-              childId: child.id,
-              activity: {
-                id: currentVideo.activityId,
-                completed: duration - time <= 30,
-                time,
-                date
-              }
-            }).then((result) => {
-              if (result.puzzle) {
-                this.player.pause()
-                this.pieceEarnedDialog = true
-                this.puzzleImg = result.puzzleImg
-              }
-            })
-          )
-        })
-        Promise.all(promises).then(() => {
-          if (promises.length) {
-            this.$nuxt.$emit('dashboard-panel-update')
-          }
-          this.savingActivityProgress = false
-        })
-      }
+      this.savingActivityProgress = true
+      this.children.forEach((child) => {
+        promises.push(
+          this.sendActivityProgress({
+            lessonId: this.lesson.id,
+            childId: child.id,
+            activity: {
+              id: currentVideo.activityId,
+              completed: duration - time <= 30,
+              time,
+              date
+            }
+          }).then((result) => {
+            if (result.puzzle) {
+              this.player.pause()
+              this.pieceEarnedDialog = true
+              this.puzzleImg = result.puzzleImg
+            }
+          })
+        )
+      })
+      Promise.all(promises).then(() => {
+        if (promises.length) {
+          this.$nuxt.$emit('dashboard-panel-update')
+        }
+        this.savingActivityProgress = false
+      })
     },
 
     completeActivityProgress () {
