2480d99a8b210672d409adca5254f9cbf4f314c2
Esteban <estebi@goldmediatech.com>
2021-05-16 19:19:12 -0500

    [added] user attribution state to the store

--- store/auth/signup/mutations.js
+++ store/auth/signup/mutations.js
@@ -1,1 +1,10 @@
-export default {}
+export default {
+  SET_UTM (state, query) {
+    state.utmSource = query.utmSource || ''
+    state.utmMedium = query.utmMedium || ''
+    state.utmCampaign = query.utmCampaign || ''
+    state.utmTerm = query.utmTerm || ''
+    state.utmName = query.utmName || ''
+    state.gc1Id = query.gc1Id || ''
+  }
+}
