ed4e08d6de5a8039864b10e93ef0cc42ea17db4d
Esteban <estebi@goldmediatech.com>
2021-05-21 10:40:41 -0500

    [added] library index test placeholder

---
+++ cypress/integration/pages/app/library/index.spec.ts
@@ -0,0 +1,32 @@
+describe('/app/library', () => {
+  it('can play featured video')
+
+  describe('categories', () => {  
+    describe('given a category is clicked', () => {
+      it('scrolls to such category')
+    })
+
+    describe('given `Play All` button is clicked', () => {
+      it('starts playing video')
+      it('can close video')
+    })
+
+    describe('given a category card is clicked', () => {
+      it('starts playing video')
+      it('can close video')
+    })
+
+    describe('given `View more` is clicked', () => {
+      it('navigates to category page')
+    })
+  })
+
+  describe('favorites', () => {
+    it('can unset favorite videos')
+    it('can set videos as favorite')
+
+    describe('given `View more` is clicked', () => {
+      it('navigates to `library/favorite`')
+    })
+  })
+})
