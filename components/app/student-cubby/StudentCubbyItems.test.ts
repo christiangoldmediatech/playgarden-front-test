c0ed025c3a9a651152fdb607d45312d495083242
Esteban <estebi@goldmediatech.com>
2021-05-27 21:23:18 -0500

    [added] tests for student cubby

--- components/app/student-cubby/StudentCubbyItems.test.ts
+++ components/app/student-cubby/StudentCubbyItems.test.ts
@@ -1,11 +1,27 @@
 import StudentCubbyItems from '@/components/app/student-cubby/StudentCubbyItems.vue'
 import { setupComponent } from '@/utils'
 
+const studentCubbyItemsList = [
+  {
+    text: 'PUZZLE',
+    title: 'STUDENTS CUBBY',
+    routeName: 'app-student-cubby-puzzle'
+  },
+  {
+    text: 'CURRICULUM',
+    title: 'Review all Curriculum',
+    routeName: 'app-student-cubby-course-progress'
+  }
+]
+
 describe('components/app/student-cubby/StudentCubbyItems.vue', () => {
   const { mountFunction } = setupComponent(StudentCubbyItems, {
+    stubs: {
+      PgSelect: true
+    },
     propsData: {
       selectedChildId: 0,
-      items: []
+      items: JSON.parse(JSON.stringify(studentCubbyItemsList))
     }
   })
 
@@ -16,12 +32,42 @@ describe('components/app/student-cubby/StudentCubbyItems.vue', () => {
   })
 
   describe('given the user is on desktop', () => {
-    it('shows desktop navigation bar')
-    it('renders each item passed as prop')
+    it('shows desktop navigation bar', () => {
+      const wrapper = mountFunction({
+        propsData: {
+          mobile: false
+        }
+      })
+
+      const bar = wrapper.find('[data-test-id=student-cubby-bar]')
+
+      expect(bar.exists()).toBeTruthy()
+    })
+
+    it('renders each item passed as prop', () => {
+      const wrapper = mountFunction({
+        propsData: {
+          mobile: false
+        }
+      })
+
+      const barItems = wrapper.findAll('[data-test-id*=student-cubby-item]')
+
+      expect(barItems.length).toEqual(studentCubbyItemsList.length)
+    })
   })
 
   describe('given the user is on mobile', () => {
-    it('shows mobile select list')
-    it('renders each item passed as prop')
+    it('shows mobile select list', () => {
+      const wrapper = mountFunction({
+        propsData: {
+          mobile: true
+        }
+      })
+
+      const select = wrapper.find('[data-test-id=student-cubby-select]')
+
+      expect(select.exists()).toBeTruthy()
+    })
   })
 })
