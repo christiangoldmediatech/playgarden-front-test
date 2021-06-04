913d78a5320d01b371d71bd90587994eb9c1bfe3
Christian Borja <Christian@Borja>
2021-06-04 15:40:33 -0500

    add changes

---
+++ components/admin/lessons-analytics/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container
+    :class="{ 'dashboard-container': !$vuetify.breakpoint.smAndDown }"
+    fluid
+  >
+    <v-main>
+      <v-row class="fill-height" justify="center">
+        <v-col cols="3">
+          panel
+        </v-col>
+        <v-col>
+          cenetr
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-container>
+</template>
---
+++ pages/admin/lessons-analytics/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-main class="bkg">
+    <v-container fluid fill-height>
+      <dashboard />
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+import Dashboard from '@/components/admin/lessons-analytics/Dashboard.vue'
+export default {
+  name: 'Index',
+
+  components: {
+    Dashboard
+  },
+
+  layout: 'admin',
+
+  data: () => ({
+    previewMode: false
+  })
+}
+</script>
