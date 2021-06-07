1c59b477b911de5427a66765c747887c807b8d6e
Esteban <estebi@goldmediatech.com>
2021-06-07 14:04:48 -0500

    [rename] composable filename

--- composables/index.ts
+++ composables/index.ts
@@ -1,5 +1,5 @@
 export * from './use-activity.composable'
-export * from './use-global-modal'
+export * from './use-global-modal.composable'
 export * from './use-library.composable'
 export * from './use-music.composable'
 export * from './helpers.composable'
