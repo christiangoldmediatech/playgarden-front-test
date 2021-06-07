64ee37e3db4af113d513269a6cf91357b0738c3b
Christian Borja <Christian@Borja>
2021-06-07 12:24:34 -0500

    add changes

--- components/admin/lessons-analytics/Dashboard.vue
+++ components/admin/lessons-analytics/Dashboard.vue
@@ -5,7 +5,11 @@
   >
     <v-row>
       <v-col cols="2">
-        panel
+        <!-- <dashboard-panel
+          v-bind="{ lesson, childId, loading }"
+          :next-button="canAdvance"
+        /> -->
+        panel two
       </v-col>
       <v-col cols="12">
         <graphics-panel />
@@ -16,17 +20,21 @@
 
 <script>
 import GraphicsPanel from '@/components/admin/lessons-analytics/GraphicsPanel.vue'
+// import DashboardPanel from '@/components/app/dashboard/DashboardPanel.vue'
 export default {
   name: 'Dashboard',
 
   components: {
     GraphicsPanel
+    // DashboardPanel
   },
 
   layout: 'admin',
 
   data: () => ({
-    previewMode: false
+    lesson: 1,
+    childId: 47,
+    loading: false
   })
 }
 </script>
