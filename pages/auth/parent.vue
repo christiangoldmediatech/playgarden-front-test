f98518c551eb7366d1653bd7fe261ba45c113da7
Esteban <estebi@goldmediatech.com>
2021-06-08 19:36:19 -0500

    [refactor] auth/parent page

--- pages/auth/parent.vue
+++ pages/auth/parent.vue
@@ -1,13 +1,13 @@
 <template>
   <v-row>
-    <v-row no-gutters>
+    <v-col cols="12">
       <v-btn
         color="accent"
         nuxt
         text
       >
         <a
-          class="d-block back mb-1 mt-1"
+          class="d-block accent--text mb-1 mt-1"
           href="https://playgardenonline.com/"
         >
           <v-icon left>
@@ -16,7 +16,7 @@
           Back
         </a>
       </v-btn>
-    </v-row>
+    </v-col>
     <v-col cols="12">
       <step-one />
     </v-col>
@@ -24,33 +24,25 @@
 </template>
 
 <script>
+import { defineComponent } from '@nuxtjs/composition-api'
 import StepOne from '@/components/app/register/StepOne'
+import { useGtmHelper } from '@/composables'
 
-export default {
+export default defineComponent({
   name: 'Parent',
 
   components: {
     StepOne
   },
 
-  data: vm => ({}),
+  setup () {
+    const gtm = useGtmHelper()
 
-  computed: {},
-
-  created () {
-    this.$gtm.push({
+    gtm.push({
       event: 'parent_page',
       conversionID: '959213252',
       conversionLabel: 'QAn5COr85PoBEMTdsckD'
     })
-  },
-
-  methods: {}
-}
+  }
+})
 </script>
-
-<style lang="scss" scoped>
-.back {
-  color: var(--v-accent-base) !important;
-}
-</style>
