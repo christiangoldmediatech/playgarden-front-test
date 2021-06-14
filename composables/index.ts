983e8e502ac950a08e643fcd7f7a24c24a80758e
Esteban <estebi@goldmediatech.com>
2021-06-14 12:26:34 -0500

    [add] signup composable to branch

--- composables/index.ts
+++ composables/index.ts
@@ -1,5 +1,6 @@
+export * from './helpers.composable'
 export * from './use-activity.composable'
 export * from './use-global-modal.composable'
 export * from './use-library.composable'
 export * from './use-music.composable'
-export * from './helpers.composable'
+export * from './use-signup.composable'
---
+++ composables/use-signup.composable.ts
@@ -0,0 +1,33 @@
+import { computed, ref, useRoute, useRouter } from '@nuxtjs/composition-api'
+import { User, UserFlow } from '@/models'
+import { axios } from '@/utils'
+
+const abFlow = ref<User['flow']>(UserFlow.CREDITCARD)
+
+export const useSignup = ({ router, route }: { router: ReturnType<typeof useRouter>, route: ReturnType<typeof useRoute> }) => {
+  const isCreditCardRequired = computed(() => abFlow.value === UserFlow.CREDITCARD)
+
+  const setupABFlow = async () => {
+    const routeABFlow = route.value.query.abf as UserFlow
+
+    if (typeof routeABFlow === 'string' && [UserFlow.NOCREDITCARD, UserFlow.CREDITCARD].includes(routeABFlow)) {
+      abFlow.value = routeABFlow
+    } else {
+      const response = await axios.$get('/auth/users/flow') as { flow: User['flow'] }
+      abFlow.value = response.flow
+    }
+
+    router.push({
+      name: 'auth-parent',
+      query: {
+        abf: abFlow.value
+      }
+    })
+  }
+
+  return {
+    abFlow,
+    isCreditCardRequired,
+    setupABFlow
+  }
+}
