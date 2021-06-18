facb30460b90d542b5d75f207351df87b76b474f
Esteban <estebi@goldmediatech.com>
2021-06-18 10:52:13 -0500

    [added] base horizontal card ribbon component

---
+++ components/ui/cards/HorizontalCardRibbon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="horizontal-card">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {}
+})
+</script>
+
+<style lang="scss" scoped>
+.horizontal-card {
+  width: 100%;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 0px 40px 40px;
+}
+</style>
