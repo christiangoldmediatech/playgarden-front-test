565a8497ac01f3801e97251065ab790b7bd71ab3
Esteban <estebi@goldmediatech.com>
2021-06-18 19:47:33 -0500

    [updated] horizontal card to be expandable

--- components/ui/cards/HorizontalCardRibbon.vue
+++ components/ui/cards/HorizontalCardRibbon.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="horizontal-card">
-    <slot />
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-expand-transition class="elevation-0">
+          <div v-show="!isMinimized">
+            <slot />
+          </div>
+        </v-expand-transition>
+      </v-col>
+      <v-col
+        cols="12"
+        class="action text-center my-2"
+        @click="$emit('update:isMinimized', !isMinimized)"
+      >
+        <v-icon>{{ isMinimized ? 'mdi-chevron-down' : 'mdi-chevron-up' }}</v-icon>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -8,15 +23,35 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup () {}
+  props: {
+    isMinimized: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props) {}
 })
 </script>
 
 <style lang="scss" scoped>
 .horizontal-card {
+  transition: 0.2s;
+  position: fixed;
+  top: 64px;
+  z-index: 50;
   width: 100%;
   background: #FFFFFF;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 0px 0px 40px 40px;
+
+  & .action {
+    cursor: pointer;
+  }
+}
+
+@media (max-width: $breakpoint-xs) {
+  .horizontal-card {
+    top: 54px;
+  }
 }
 </style>
