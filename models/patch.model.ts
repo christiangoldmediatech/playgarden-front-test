fd74342bc24f7641e14de2ffabc45fdde96df2da
Christian Borja <Christian@Borja>
2021-07-01 11:57:35 -0500

    add changes

--- models/patch.model.ts
+++ models/patch.model.ts
@@ -8,31 +8,3 @@ export interface Patch {
   patchType: string
   unblocked: boolean
 }
-
-export interface Patches {
-  color: string
-  description: string
-  icon: string
-  name: string
-  patches: Patch[]
-  toUnblock: number
-  type: {
-    activity: boolean
-    extra: boolean
-    lesson: boolean
-  }
-}
-
-export interface ChildrenPatchesActivity {
-  name: string
-  description: string
-  type: {
-      lesson: boolean
-      extra: boolean
-      activity: boolean
-  },
-  icon: string
-  color: string
-  toUnloock: number
-  patches: Array<Patches>
-}
