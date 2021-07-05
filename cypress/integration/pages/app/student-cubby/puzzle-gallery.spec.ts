7a32753ced3c0297379f81fefa77c12cd5ae74e6
Christian Borja <Christian@Borja>
2021-07-04 23:50:57 -0500

    add changes

--- cypress/integration/pages/app/student-cubby/puzzle-gallery.spec.ts
+++ cypress/integration/pages/app/student-cubby/puzzle-gallery.spec.ts
@@ -5,8 +5,11 @@ describe('/app/student-cubby', () => {
         cy.headlessChildSelect(children[0].id)
         cy.visit('/app/dashboard')
         cy.get('[data-test-id=app-student-cubby]').click()
-        cy.get('body')
       })
+    })
+
+    it('loads the page', () => {
+      cy.url().should('include', 'app/student-cubby/puzzle')
     })
 
     describe('given PUZZLE is clicked', () => {
