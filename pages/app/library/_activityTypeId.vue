c4fc2cda2575c6620f0af4fa691b3f31ae3b9c9b
Esteban <estebi@goldmediatech.com>
2021-07-07 17:46:16 -0500

    [remove] featured video from activity category

--- pages/app/library/_activityTypeId.vue
+++ pages/app/library/_activityTypeId.vue
@@ -1,12 +1,5 @@
 <template>
   <v-main class="pos-relative">
-    <featured-video
-      v-if="hasFeaturedVideo"
-      :video="featuredById"
-      go-back
-      @play="playFeaturedVideo"
-    />
-
     <v-btn
       class="activity-type-back"
       text
@@ -46,7 +39,6 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, useRoute, computed } from '@nuxtjs/composition-api'
-import FeaturedVideo from '@/components/app/library/FeaturedVideo.vue'
 import ActivityTypeContainer from '@/components/app/library/ActivityTypeContainer.vue'
 import ActivityTypeHeader from '@/components/app/library/ActivityTypeHeader.vue'
 import ActivityPlayer from '@/components/app/activities/ActivityPlayer.vue'
@@ -56,7 +48,6 @@ export default defineComponent({
   name: 'ActivityTypeId',
 
   components: {
-    FeaturedVideo,
     ActivityTypeContainer,
     ActivityTypeHeader,
     ActivityPlayer
@@ -66,11 +57,10 @@ export default defineComponent({
     const route = useRoute()
     const loading = ref(false)
 
-    const { activityById, featuredById, totalById, getActivitiesById } = useActivity()
+    const { activityById, totalById, getActivitiesById } = useActivity()
     const { getAllFavorites } = useLibrary()
 
     const id = computed(() => parseInt(route.value.params.activityTypeId))
-    const hasFeaturedVideo = computed(() => !!Object.keys(featuredById.value || {}).length)
     const hasActivity = computed(() => !!Object.keys(activityById.value || {}).length)
 
     onMounted(async () => {
@@ -88,28 +78,12 @@ export default defineComponent({
       })
     }
 
-    const playFeaturedVideo = () => {
-      const featuredId = featuredById.value.id
-      const playlist = activityById.value.playlist
-
-      if (!playlist) {
-        return
-      }
-
-      const index = playlist.findIndex(playItem => playItem.activityId === featuredId)
-
-      ctx.root.$nuxt.$emit('open-activity-player', { playlist, index })
-    }
-
     return {
       loading,
       activityById,
-      featuredById,
       totalById,
       hasActivity,
-      hasFeaturedVideo,
-      handlePlayAll,
-      playFeaturedVideo
+      handlePlayAll
     }
   }
 })
