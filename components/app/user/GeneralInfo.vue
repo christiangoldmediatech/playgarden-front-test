0d737ea2d7c03eda36dab3fca709bf2b1a04162f
Christian Borja <Christian@Borja>
2021-07-12 17:06:56 -0500

    add changes

--- components/app/user/GeneralInfo.vue
+++ components/app/user/GeneralInfo.vue
@@ -195,6 +195,7 @@ export default {
   data: () => ({
     form: {},
     isEditing: false,
+    oldEmail: '',
     passwordModal: false,
     loading: false
   }),
@@ -215,6 +216,7 @@ export default {
       deep: true,
       handler () {
         this.form = { ...this.userInfo }
+        this.oldEmail = this.userInfo.email
       }
     }
   },
@@ -315,6 +317,9 @@ export default {
           email: this.form.email,
           phoneNumber: this.form.phoneNumber
         })
+      } catch (error) {
+        console.log('this.olf--', this.oldEmail)
+        this.form.email = this.oldEmail
       } finally {
         this.loading = false
         this.isEditing = false
