046150f3e99e39f72be132d87314a4a41e97c233
Christian Borja <Christian@Borja>
2021-08-09 12:17:57 -0500

    Adding class style

--- components/app/playdates/WillBeBack.vue
+++ components/app/playdates/WillBeBack.vue
@@ -18,7 +18,7 @@
           </v-col>
 
           <v-col cols="12" class="text-md-left text-center">
-            <p class="text-justify">
+            <p class="text-justify text-info-playdate">
               As we work to bring the playdates back, you can go to the library and watch a lot of great videos with your Playgarden Prep teachers.
             </p>
           </v-col>
@@ -52,3 +52,9 @@ export default defineComponent({
   name: 'WillBeBack'
 })
 </script>
+
+<style lang="scss" scoped>
+.text-info-playdate {
+    font-size: 22px !important;
+}
+</style>
