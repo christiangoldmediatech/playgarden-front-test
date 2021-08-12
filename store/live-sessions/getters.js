51c601a46055946138dd05730b1eed3059269af7
Jesse Angell <jesse.angell.1990@gmail.com>
2021-08-11 21:27:11 -0500

    Reduces number of hours to last hour + 1

--- store/live-sessions/getters.js
+++ store/live-sessions/getters.js
@@ -34,6 +34,7 @@ export default {
     // Create default base obj
     const schedule = {
       firstHour: 0,
+      endHour: 18,
       days
     }
 
@@ -44,11 +45,13 @@ export default {
     // Get the sessions and transform them for easier sorting
     const sessions = state.sessions.map((session) => {
       const date = new Date(session.dateStart)
+      const endDate = new Date(session.dateEnd)
       return {
         ...session,
         weekDay: date.getDay(),
         day: date.getDate(),
-        hour: date.getHours()
+        hour: date.getHours(),
+        endHour: endDate.getHours()
       }
     }).sort((a, b) => {
       return a.day - b.day || a.hour - b.hour
@@ -56,19 +59,34 @@ export default {
 
     schedule.sessions = sessions
 
-    // Find first hour.
+    // Find first start and end hour.
     if ((sessions[0].hour - 1) > 0) {
       schedule.firstHour = sessions[0].hour - 1
     } else {
       schedule.firstHour = sessions[0].hour
     }
 
+    if (sessions[0].endHour === 23) {
+      schedule.endHour = 23
+    } else {
+      schedule.endHour = sessions[0].endHour + 1
+    }
+
+    // Now find start and end hours for all sessions
     sessions.forEach((session) => {
       if (session.hour < schedule.firstHour) {
         if (session.hour > 0) {
           schedule.firstHour = session.hour - 1
         } else {
           schedule.firstHour = session.hour
+        }
+      }
+
+      if ((session.endHour + 1) > schedule.endHour) {
+        if (session.endHour === 23) {
+          schedule.endHour = 23
+        } else {
+          schedule.endHour = session.endHour + 1
         }
       }
     })
