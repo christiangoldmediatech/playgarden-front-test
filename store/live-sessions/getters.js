0babba0d2056b17360f11fd59d17c7e1000abd17
Jesse Angell <jesse.angell.1990@gmail.com>
2021-08-13 08:05:17 -0500

    Adds requested comment

--- store/live-sessions/getters.js
+++ store/live-sessions/getters.js
@@ -59,13 +59,20 @@ export default {
 
     schedule.sessions = sessions
 
-    // Find first start and end hour.
+    // Find first start and end hour. We need to set real initial value
+    // so that when we loop through all the sessions, we can find the correct hours
+    // This is what allows us to have our initial empty placeholder values when no sessions
+    // loaded, and allows an empty table to render correctly.
+    // We want the first hour to be earlier than the first hour used,
+    // But not earlier than 0 hour
     if ((sessions[0].hour - 1) > 0) {
       schedule.firstHour = sessions[0].hour - 1
     } else {
       schedule.firstHour = sessions[0].hour
     }
 
+    // The same applies to the end hour; one hour later and the last used,
+    // but not greater than 23
     if (sessions[0].endHour === 23) {
       schedule.endHour = 23
     } else {
@@ -73,6 +80,7 @@ export default {
     }
 
     // Now find start and end hours for all sessions
+    // We will use the same logic as above, but not for each session in a loop
     sessions.forEach((session) => {
       if (session.hour < schedule.firstHour) {
         if (session.hour > 0) {
