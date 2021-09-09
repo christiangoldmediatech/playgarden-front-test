3e290e617ccb888f6ab1df3df4edd284b489de3d
Christian Borja <Christian@Borja>
2021-09-09 11:26:22 -0500

    deleting console

--- store/admin/children/actions.js
+++ store/admin/children/actions.js
@@ -1,7 +1,6 @@
 export default {
   async getChildrensProgress ({ commit }, params) {
     const data = await this.$axios.$get('/children/progress/list', { params })
-    console.log('data--', data)
     commit('SET_ROWS', data.children)
     commit('SET_FILTERED', data.filtered)
     commit('SET_LIMIT', data.limit)
