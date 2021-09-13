2c48144bfeaec4878dd757ce1b14bb961ae2ade8
Jesse Angell <jesse.angell.1990@gmail.com>
2021-09-13 11:04:04 -0500

    Fixes player issues

--- pages/birthday-wish.vue
+++ pages/birthday-wish.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <p>dddd</p>
-    <pg-video-player inline @ready="onPlayerReady" />
-    <p>dddd</p>
-  </div>
+  <v-container>
+    <v-row>
+      <pg-video-player inline @ready="onPlayerReady" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// import VideoPreviewBtn from '@/components/admin/video-preview/VideoPreviewBtn.vue'
 export default {
   name: 'BirthdayWish',
   data: () => {
@@ -20,12 +19,16 @@ export default {
   methods: {
     onPlayerReady (player) {
       this.player = player
-      const t = {
-        url: '/test.mp4',
-        type: 'video/mp4'
+
+      const mediaObject = {
+        title: 'ss',
+        src: {
+          url: 'https://d3dnpqxalhovr4.cloudfront.net/out/v1/a641438ecab74f9eb3dd1b4e92210988/748ec5dbba9f4aa0a2eef8a74fb2c043/68b7491c440d41d4b8b6fb0ae08fe0b4/index.m3u8',
+          type: 'application/x-mpegURL'
+        }
       }
 
-      player.loadPlaylist([{ title: 'ss', src: t }])
+      player.loadPlaylist([mediaObject])
     }
   }
 }
