9c053cf6fac1ded09469ec464718c56f0a1d1c68
grover plaza quiroga <grover.plaza@gmail.com>
2021-09-13 11:45:19 -0400

    latest

---
+++ pages/birthday-wish.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>dddd</p>
+    <pg-video-player inline @ready="onPlayerReady" />
+    <p>dddd</p>
+  </div>
+</template>
+
+<script>
+// import VideoPreviewBtn from '@/components/admin/video-preview/VideoPreviewBtn.vue'
+export default {
+  name: 'BirthdayWish',
+  data: () => {
+    return {
+      dialog: false,
+      title: '',
+      player: null
+    }
+  },
+  methods: {
+    onPlayerReady (player) {
+      this.player = player
+      const t = {
+        url: '/test.mp4',
+        type: 'video/mp4'
+      }
+
+      player.loadPlaylist([{ title: 'ss', src: t }])
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
