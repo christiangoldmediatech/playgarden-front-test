5925f560ebf0c571df03045eb55d4f0050ba8cf0
grover plaza quiroga <grover.plaza@gmail.com>
2021-09-16 15:05:57 -0400

    css in scoped

--- components/holydays/birthday/model/index.vue
+++ components/holydays/birthday/model/index.vue
@@ -13,8 +13,10 @@
 </template>
 
 <script lang="ts">
+import { PlayerInstance } from '@gold-media-tech/pg-video-player/src/types/PlayerInstance'
+import { MediaObject } from '@gold-media-tech/pg-video-player/src/types/MediaObject'
 import BirthdayContentDialog from '@/components/ui/dialogs/LargeImageContentDialog/BirthdayContentDialog.vue'
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   name: 'BirthdayWishes',
@@ -22,24 +24,23 @@ export default defineComponent({
   components: {
     BirthdayContentDialog
   },
-  data: () => {
-    return {
-      dialog: false,
-      title: '',
-      player: null
-    }
-  },
-  methods: {
-    onPlayerReady (player:any) {
-      this.player = player
-      const mediaObject = {
-        title: 'ss',
+  setup () {
+    const dialog = ref(false)
+    const title = ref('')
+    const onPlayerReady = (player: PlayerInstance) => {
+      const mediaObject: MediaObject = {
+        title: 'Happy Birthday',
         src: {
           url: 'https://d3dnpqxalhovr4.cloudfront.net/out/v1/a641438ecab74f9eb3dd1b4e92210988/748ec5dbba9f4aa0a2eef8a74fb2c043/68b7491c440d41d4b8b6fb0ae08fe0b4/index.m3u8',
           type: 'application/x-mpegURL'
         }
       }
       player.loadPlaylist([mediaObject])
+    }
+    return {
+      dialog,
+      title,
+      onPlayerReady
     }
   }
 })
--- components/ui/dialogs/LargeImageContentDialog/BirthdayContentDialog.vue
+++ components/ui/dialogs/LargeImageContentDialog/BirthdayContentDialog.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-dialog content-class="w-10" persistent :fullscreen="isMobile" :value="value">
-    <v-card :class="{ 'border-4': !isMobile }">
-      <v-row no-gutters>
-        <v-col cols="12" md="12" class="topm">
-          <!-- Desktop Close Button -->
-          <v-btn v-if="isCloseable" class="d-none d-md-inline float-right" icon data-test-id="desktop-close-button" @click="$emit('close')">
-            <v-icon size="32" color="white">
-              mdi-close
-            </v-icon>
-          </v-btn>
+  <div class="container-modal">
+    <v-dialog class="test" content-class="w-10" persistent :fullscreen="isMobile" :value="value">
+      <v-card :class="{ 'border-4': !isMobile }">
+        <v-row no-gutters>
+          <v-col cols="12" md="12" class="topm">
+            <!-- Desktop Close Button -->
+            <v-btn v-if="isCloseable" class="d-none d-md-inline float-right" icon data-test-id="desktop-close-button" @click="$emit('close')">
+              <v-icon size="32" color="white">
+                mdi-close
+              </v-icon>
+            </v-btn>
 
-          <!-- Mobile Close Button -->
-          <v-btn v-if="isCloseable" class="d-md-none warning--text mb-4 pl-0" text data-test-id="mobile-close-button" @click="$emit('close')">
-            <v-icon left>
-              mdi-arrow-left
-            </v-icon>
-            Back
-          </v-btn>
+            <!-- Mobile Close Button -->
+            <v-btn v-if="isCloseable" class="d-md-none warning--text mb-4 pl-0" text data-test-id="mobile-close-button" @click="$emit('close')">
+              <v-icon left>
+                mdi-arrow-left
+              </v-icon>
+              Back
+            </v-btn>
 
-          <!-- Modal Body -->
-          <slot name="default" />
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-dialog>
+            <!-- Modal Body -->
+            <slot name="default" />
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -69,19 +71,19 @@ export default {
   overflow-y:visible
 }
 @media screen and (max-width: 2500px) {
-  .w-10.v-dialog:not(.v-dialog--fullscreen) {
+  .container-modal >>> .w-10.v-dialog:not(.v-dialog--fullscreen) {
     width: 1200px;
   }
 }
 @media screen and (max-width: 1600px) {
-  .w-10.v-dialog:not(.v-dialog--fullscreen) {
+  .container-modal >>> .w-10.v-dialog:not(.v-dialog--fullscreen) {
     width: 750px !important;
   }
 }
 
 /* On screens that are 600px wide or less, make stack on top of each other instead of next to each other */
 @media screen and (max-width: 1367px) {
-  .w-10.v-dialog:not(.v-dialog--fullscreen) {
+  .container-modal >>> .w-10.v-dialog:not(.v-dialog--fullscreen) {
     width: 600px !important;
   }
 }
