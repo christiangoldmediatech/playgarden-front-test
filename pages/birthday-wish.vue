27220c01e4e435ebb91ea08a8142503277fcf679
Jesse Angell <jesse.angell.1990@gmail.com>
2021-09-16 10:43:48 -0500

    Convert to composition api

--- pages/birthday-wish.vue
+++ pages/birthday-wish.vue
@@ -6,22 +6,22 @@
   </v-container>
 </template>
 
-<script>
-export default {
-  layout: 'public-birthday',
-  name: 'BirthdayWish',
-  data: () => {
-    return {
-      dialog: false,
-      title: '',
-      player: null
-    }
-  },
-  methods: {
-    onPlayerReady (player) {
-      this.player = player
+<script lang="ts">
+import { ref, defineComponent } from '@nuxtjs/composition-api'
+import { PlayerInstance } from '@gold-media-tech/pg-video-player/src/types/PlayerInstance'
+import { MediaObject } from '@gold-media-tech/pg-video-player/src/types/MediaObject'
 
-      const mediaObject = {
+export default defineComponent({
+  name: 'BirthdayWish',
+
+  layout: 'public-birthday',
+
+  setup () {
+    const dialog = ref(false)
+    const title = ref('')
+
+    const onPlayerReady = (player: PlayerInstance) => {
+      const mediaObject: MediaObject = {
         title: 'ss',
         src: {
           url: 'https://d3dnpqxalhovr4.cloudfront.net/out/v1/a641438ecab74f9eb3dd1b4e92210988/748ec5dbba9f4aa0a2eef8a74fb2c043/68b7491c440d41d4b8b6fb0ae08fe0b4/index.m3u8',
@@ -31,8 +31,14 @@ export default {
 
       player.loadPlaylist([mediaObject])
     }
+
+    return {
+      dialog,
+      title,
+      onPlayerReady
+    }
   }
-}
+})
 </script>
 
 <style>
