bb653ef162dc9600cbaa26c673b545b5b6459deb
Juan Moscoso <juan.moscoso@primme.tech>
2021-09-21 12:16:05 -0500

    changes to route

--- pages/happy-birthday/_id.vue
+++ pages/happy-birthday/_id.vue
@@ -22,10 +22,10 @@ export default defineComponent({
 
     const onPlayerReady = (player: PlayerInstance) => {
       const mediaObject: MediaObject = {
-        title: 'ss',
+        title: 'Birthday Wish',
         src: {
-          url: 'https://d3dnpqxalhovr4.cloudfront.net/out/v1/a641438ecab74f9eb3dd1b4e92210988/748ec5dbba9f4aa0a2eef8a74fb2c043/68b7491c440d41d4b8b6fb0ae08fe0b4/index.m3u8',
-          type: 'application/x-mpegURL'
+          url: 'https://video-on-demand-stack-prod-source-1rd9pvb7kaoed.s3.amazonaws.com/parts/activity-video/1a596269-7a85-47e7-98c1-c16f396f9249.mp4',
+          type: 'video/mp4'
         }
       }
 
