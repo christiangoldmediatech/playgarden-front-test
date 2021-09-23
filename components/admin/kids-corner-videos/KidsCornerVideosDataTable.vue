64e130939f09f55b55f66a95cc660960d439e9b1
christiangoldmediatech <69595015+christiangoldmediatech@users.noreply.github.com>
2021-09-23 18:03:40 -0500

    adding new views

---
+++ components/admin/kids-corner-videos/KidsCornerVideosDataTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card width="100%">
+          <v-card-title>
+            Kids corner videos - Management
+
+            <v-spacer />
+
+            <v-btn
+              class="mr-2 text-none"
+              color="primary darken-1"
+              dark
+              :icon="$vuetify.breakpoint.xs"
+              nuxt
+              :to="{ name: 'admin-kids-corner-videos-editor' }"
+            >
+              <v-icon class="hidden-sm-and-up">
+                mdi-plus-circle
+              </v-icon>
+
+              <v-icon class="hidden-xs-only" small>
+                mdi-plus
+              </v-icon>
+              <span class="hidden-xs-only white--text">
+                Add new kids corner video
+              </span>
+            </v-btn>
+          </v-card-title>
+
+          <v-card-text>
+            View, create, update, or delete kids corner videos.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-card width="100%">
+      <v-card-text>
+        <v-row>
+          list kids videos
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'defineComponent'
+})
+</script>
---
+++ components/admin/kids-corner-videos/KidsCornerVideosEditor.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card width="100%">
+          <v-card-title>
+            New Kids corner videos
+
+            <v-spacer />
+
+            <v-btn
+              class="mr-2 text-none"
+              color="accent darken-1"
+              dark
+              :icon="$vuetify.breakpoint.xs"
+              nuxt
+              :to="{ name: 'admin-kids-corner-videos-editor' }"
+            >
+              <span class="hidden-xs-only white--text">
+                Back
+              </span>
+            </v-btn>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-card width="100%">
+      <v-card-text>
+        <v-row>
+          <v-col cols="8">
+            <v-row>
+              <v-col class="text-md-right" cols="12" sm="3">
+                <span class="subheader font-weight-bold">Name:</span>
+              </v-col>
+
+              <v-col cols="12" sm="9" lg="6">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Name"
+                  rules="required"
+                >
+                  <pg-text-field
+                    :error-messages="errors"
+                    label="Name"
+                    solo-labeled
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col class="text-md-right" cols="12" sm="3">
+                <span class="subheader font-weight-bold">Description:</span>
+              </v-col>
+
+              <v-col cols="12" sm="9" lg="6">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Description"
+                  rules="required"
+                >
+                  <pg-textarea
+                    :error-messages="errors"
+                    label="Description"
+                    solo-labeled
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col class="text-md-right" cols="12" sm="3">
+                <span class="subheader">Category:</span>
+              </v-col>
+
+              <v-col cols="12" sm="9" lg="6">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Category"
+                  rules="required"
+                >
+                  <pg-select
+                    :error-messages="errors"
+                    label="Select a category"
+                    solo-labeled
+                  />
+                </validation-provider>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col class="text-md-right" cols="12" sm="3">
+                <span class="subheader">Curriculum:</span>
+              </v-col>
+
+              <v-col cols="12" sm="9" lg="6">
+                <pg-select
+                  label="Curriculum"
+                  solo-labeled
+                />
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            images
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'KidsCornerVideosEditor',
+  setup () {
+
+  }
+})
+</script>
