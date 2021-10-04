d8799eb7fe58901fd5881048ea0b7a94884afeed
Alosies George <alosies@gmail.com>
2021-10-04 09:02:28 +0530

    WIP

---
+++ pages/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <SubscriptionEndDialog />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import SubscriptionEndDialog from '@/components/ui/dialogs/SubscriptionEndDialog/index.vue'
+
+export default defineComponent({
+  name: 'TestPage',
+  components: {
+    SubscriptionEndDialog
+  },
+  setup () {
+    const reactiveData = ref('this is composed')
+
+    return {
+      reactiveData
+    }
+  }
+})
+</script>
+
+<style lang="saas" scoped></style>
