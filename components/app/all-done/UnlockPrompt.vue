b4ee01da9d956c9e5fab7d741a46f38d274d8a79
Alosies George <alosies@gmail.com>
2021-11-11 19:08:59 +0530

    Cleared bg-image
    Cleanup

--- components/app/all-done/UnlockPrompt.vue
+++ components/app/all-done/UnlockPrompt.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="overlay bg-image">
-    <main class="overlay overlayContent">
-      <section class="promptMessage">
-        <div class="d-flex align-center">
-          <img
-            :src="require('@/assets/png/person-with-laptop.png')"
-            alt="lessons image"
-            height="60"
-          >
-          <div class="text-h5 text-md-h4 ml-4">
-            DAILY LESSONS
-          </div>
+  <main class="overlay">
+    <section class="promptMessage">
+      <div class="d-flex align-center">
+        <img
+          :src="require('@/assets/png/person-with-laptop.png')"
+          alt="lessons image"
+          height="60"
+        >
+        <div class="text-h5 text-md-h4 ml-4">
+          DAILY LESSONS
         </div>
+      </div>
 
-        <p class="mt-4 text-center">
-          Upgrade your plan to have access to daily lessons with your favorite playgarden prep teachers
-        </p>
-      </section>
-      <PlanUpgradePrompt />
-    </main>
-  </div>
+      <p class="mt-4 text-center">
+        Upgrade your plan to have access to daily lessons with your favorite playgarden prep teachers
+      </p>
+    </section>
+    <PlanUpgradePrompt />
+  </main>
 </template>
 
 <script lang="ts">
@@ -35,27 +33,13 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.bg-image {
-  background-image: url('~assets/png/lessons-image.png');
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-attachment: fixed;
-  background-size: cover;
-}
-
 .overlay {
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  z-index: 6;
-  min-height: 500px;
-}
-
-.overlayContent {
   z-index: 7;
-  background-color: #80808000;
   background-color: rgb(0, 0, 0, 0.6);
   color: white;
   min-height: 500px;
@@ -68,8 +52,9 @@ export default defineComponent({
   margin-bottom: 20px;
 
   p {
-    max-width: 520px;
+    max-width: 550px;
     font-size: 20px;
+    padding: 1rem;
 
     @media (max-width: $breakpoint-sm) {
       line-height: 20px;
