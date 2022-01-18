f82293235fa7aad4b0d4b8c50015f814bda241fb
Esteban <estebi@goldmediatech.com>
2022-01-18 09:15:25 -0500

    [update] shipping address content

--- components/app/payment/ShippingAddressModal.vue
+++ components/app/payment/ShippingAddressModal.vue
@@ -2,19 +2,19 @@
   <large-image-content-dialog
     :value="isShippingModalVisible"
     :img="girlFamilyTreeImg"
-    :image-height="'500px'"
+    :fullscreen="false"
     @close="dontShowAgain"
   >
     <div class="text-center">
       <underlined-title
         text="We want to send you a Playgarden Prep workbook!"
         font-size="46px"
-        font-size-mobile="22px"
+        font-size-mobile="20px"
         letter-spacing="4.8px"
       />
     </div>
 
-    <div class="grey--text text--darken-2 text-md-h5 my-3 my-md-6 text-center">
+    <div class="grey--text text--darken-2 mt-4 mb-3 my-md-6 text-center pg-text-[17px] md:pg-text-[26px]">
       In order to receive your FREE A-D workbook, please provide your shipping
       address by going to your Accounts Page
       <nuxt-link
@@ -29,7 +29,7 @@
       <v-btn
         color="secondary"
         class="my-3"
-        x-large
+        :x-large="!isMobile"
         :width="isMobile ? '90%' : '500px'"
         @click="closeModal"
       >
@@ -39,15 +39,15 @@
       <v-btn
         color="accent"
         text
-        x-large
         class="mb-3 text-decoration-underline"
+        :x-large="!isMobile"
         :width="isMobile ? '90%' : '500px'"
         @click="dontShowAgain"
       >
         DON'T SHOW ME THIS AGAIN
       </v-btn>
 
-      <div class="grey--text text--darken-2">
+      <div class="grey--text text--darken-2 caption text-md-body-1 text-center">
         *Limited to 1 introductory learning package per family, in the
         territorial US and Canada only.
       </div>
@@ -55,7 +55,7 @@
   </large-image-content-dialog>
 </template>
 
-<script>
+<script lang="ts">
 import { mapState } from 'vuex'
 import LargeImageContentDialog from '@/components/ui/dialogs/LargeImageContentDialog/LargeImageContentDialog.vue'
 
@@ -73,21 +73,33 @@ export default {
   computed: {
     ...mapState('notifications', ['isShippingModalVisible']),
 
-    isMobile() {
+    isMobile(): boolean {
       return this.$vuetify.breakpoint.smAndDown
     }
   },
 
   methods: {
-    dontShowAgain() {
+    dontShowAgain(): void {
       this.$store.commit('notifications/SET_IS_SHIPPING_MODAL_VISIBLE', false)
       this.$store.dispatch('notifications/showShippingAddressModalAgain', false)
     },
 
-    closeModal() {
+    closeModal(): void {
       this.$store.commit('notifications/SET_IS_SHIPPING_MODAL_VISIBLE', false)
       this.$store.dispatch('notifications/showShippingAddressModalAgain', true)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.pg-text-\[17px\] {
+  font-size: 17px;
+}
+
+@media(min-width: $breakpoint-md) {
+  .md\:pg-text-\[26px\] {
+    font-size: 26px;
+  }
+}
+</style>
