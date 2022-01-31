01ae7e8db743bf791f8fec2fdfc325aed9c076de
Esteban <estebi@goldmediatech.com>
2022-01-31 10:20:46 -0500

    [added] email conflict modal

---
+++ components/app/register/EmailConflictModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <large-image-content-dialog :fullscreen="false" :value="isEmailConflictModalVisible" :img="img" :is-closeable="true" @close="closeModal">
+      <div class="pg-text-center">
+        <underlined-title
+          text="UH-OH! LOOKS LIKE THIS EMAIL IS ALREADY IN USE."
+          font-size="46px"
+          font-size-mobile="24px"
+          letter-spacing="4.8px"
+          class="pg-text-pg-grey"
+        />
+      </div>
+
+      <p class="pg-text-center pg-text-xl md:pg-text-4xl pg-text-pg-grey pg-mt-6">
+        Click <span class="pg-text-accent pg-underline pg-font-bold" @click="handleHereClick">HERE</span> to Login into your account and finish the registration process
+      </p>
+    </large-image-content-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useStore, computed } from '@nuxtjs/composition-api'
+import LargeImageContentDialog from '@/components/ui/dialogs/LargeImageContentDialog/LargeImageContentDialog.vue'
+import { TypedStore } from '@/models'
+import { useNotification } from '@/composables'
+
+export default defineComponent({
+  name: 'EmailConflictModal',
+
+  components: {
+    LargeImageContentDialog
+  },
+
+  setup() {
+    const store = useStore<TypedStore>()
+    const { setIsEmailConflictModalVisible } = useNotification({ store })
+    const img = require('@/assets/png/child-magnifying-glass.png')
+
+    const isEmailConflictModalVisible = computed(() => {
+      return store.state.notifications.isEmailConflictModalVisible
+    })
+
+    function closeModal() {
+      setIsEmailConflictModalVisible(false)
+    }
+
+    function handleHereClick() {}
+
+    return {
+      img,
+      isEmailConflictModalVisible,
+      closeModal,
+      handleHereClick
+    }
+  }
+})
+</script>
