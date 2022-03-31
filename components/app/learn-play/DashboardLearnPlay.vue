3ba97fd006107814c5b47b4c0c6c03a8d81ccbc7
christiangoldmediatech <69595015+christiangoldmediatech@users.noreply.github.com>
2022-03-31 17:45:21 -0500

    Adding learn and play

---
+++ components/app/learn-play/DashboardLearnPlay.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row>
+    dashboar
+  </v-row>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'DashboardLearnPlay',
+  setup() {
+
+  }
+})
+</script>
---
+++ components/app/learn-play/LearnPlayView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col cols="4">
+      menu
+    </v-col>
+    <v-col cols="8">
+      <dashboard-learn-play />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import DashboardLearnPlay from '@/components/app/learn-play/DashboardLearnPlay.vue'
+
+export default defineComponent({
+  name: 'LearnPlayView',
+  components: {
+    DashboardLearnPlay
+  },
+  setup() {
+
+  }
+})
+</script>
