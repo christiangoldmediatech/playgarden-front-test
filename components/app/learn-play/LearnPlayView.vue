f6a4437204653b7ca613081a73eab3d8a9403e6f
christiangoldmediatech <69595015+christiangoldmediatech@users.noreply.github.com>
2022-04-04 00:46:56 -0500

    Adding menu

--- components/app/learn-play/LearnPlayView.vue
+++ components/app/learn-play/LearnPlayView.vue
@@ -1,9 +1,9 @@
 <template>
   <v-row>
-    <v-col cols="2">
-      menu
+    <v-col cols="1">
+      <menu-learn-play />
     </v-col>
-    <v-col cols="10">
+    <v-col cols="11">
       <dashboard-learn-play />
     </v-col>
   </v-row>
@@ -12,11 +12,13 @@
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import DashboardLearnPlay from '@/components/app/learn-play/DashboardLearnPlay.vue'
+import MenuLearnPlay from '@/components/app/learn-play/MenuLearnPlay.vue'
 
 export default defineComponent({
   name: 'LearnPlayView',
   components: {
-    DashboardLearnPlay
+    DashboardLearnPlay,
+    MenuLearnPlay
   },
   setup() {
 
---
+++ components/app/learn-play/MenuLearnPlay.vue
@@ -0,0 +1,134 @@
+<template>
+  <v-row class="ml-n15">
+    <v-col cols="12" class="ml-n6">
+      <img
+        v-if="child"
+        :alt="child.backpack.name"
+        class="backpack-active"
+        :src="child.backpack.image"
+      >
+      <span class="ml-8 font-weight-bold">
+        {{ child.firstName }}
+      </span>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Sections
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Video Lessons
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Worksheets
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        DIY Project
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Art Project
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Snack with Description
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Book
+      </nuxt-link>
+    </v-col>
+    <v-col cols="12">
+      <nuxt-link to="/secction">
+        Playlist
+      </nuxt-link>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'MenuLearnPlay',
+  components: {},
+  data: () => {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    ...mapGetters({ currentChild: 'getCurrentChild' }),
+    ...mapGetters('admin/curriculum', { lesson: 'getLesson' }),
+    ...mapGetters('children/lesson', {
+      currentLessonId: 'getCurrentLessonId'
+    }),
+    childrenIds () {
+      return (this.currentChild && this.currentChild.length) ? this.currentChild[0].id : 0
+    },
+    child () {
+      return (this.currentChild && this.currentChild.length) ? this.currentChild[0] : null
+    },
+    curriculumTypeId () {
+      if (this.lesson && this.lesson.curriculumType) {
+        return this.lesson.curriculumType.id
+      } else {
+        return null
+      }
+    },
+    currentVideo () {
+      return (this.lesson && this.lesson.videos.length > 0) ? this.lesson.videos[0] : { videoUrl: null }
+    },
+    getOfflineWorksheet() {
+      if (this.lesson) {
+        return this.lesson.worksheets.filter(({ type }) => type === 'OFFLINE')
+      }
+      return []
+    }
+  },
+  async created () {
+    await this.getAllChildren()
+    console.log('currentChild---', this.currentChild)
+    await this.handleLesson()
+  },
+  methods: {
+    ...mapActions('children', { getAllChildren: 'get' }),
+    ...mapActions('children/lesson', [
+      'getCurrentLesson',
+      'getCurrentLessonByChildrenId',
+      'resetChild'
+    ]),
+
+    changeChild (newId, redirect = true) {
+      const child = this.allChildren.find(({ id }) => id === parseInt(newId))
+      this.setChild({ value: [child], save: true })
+      if (redirect) {
+        this.handleLesson(true).then(() => {
+          // this.$router.push({ name: 'app-dashboard' })
+          // this.redirectDashboard()
+        })
+      }
+    },
+    async handleLesson () {
+      try {
+        this.loading = true
+        await this.getCurrentLesson({
+          childrenIds: this.childrenIds
+        })
+      } catch (error) {
+        return Promise.reject(error)
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
