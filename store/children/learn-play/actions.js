2bcded58b6f650d52d9fc19157a404132b93d905
christiangoldmediatech <69595015+christiangoldmediatech@users.noreply.github.com>
2022-04-19 17:46:36 -0500

    adding new path endpoint

--- store/children/learn-play/actions.js
+++ store/children/learn-play/actions.js
@@ -5,7 +5,7 @@ export default {
   getFirstLearnPlay ({ commit }, params) {
     return new Promise((resolve, reject) =>
       this.$axios
-        .$get('/play-and-learn/get/first', { params })
+        .$get('/play-and-learn/get/current', { params })
         .then(resolve)
         .catch((error) => {
           snotifyError(commit, {
