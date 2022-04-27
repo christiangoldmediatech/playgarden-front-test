ee5fdf771e72c3c908e21130f85ba7632fc16d1e
Esteban <esteban.lds@outlook.com>
2022-04-27 12:18:30 -0500

    [wip] footer

--- components/app/gift-of-learning/Footer/Footer.vue
+++ components/app/gift-of-learning/Footer/Footer.vue
@@ -1,13 +1,63 @@
 <template>
-  <v-footer app>
-    Footer
+  <v-footer
+    :class="[
+      'pg-bg-[url(@/assets/png/gift-of-learning/bottom-paper.png)]',
+      'pg-bg-cover',
+      'pg-bg-top',
+    ]"
+    app
+    absolute
+    color="transparent"
+  >
+    <div
+      :class="[
+        'pg-grid',
+        'pg-grid-cols-1',
+        'md:pg-grid-cols-6',
+        'pg-max-w-[1150px]',
+        'pg-mx-auto'
+      ]"
+    >
+      <!-- LOGO -->
+      <div class="pg-flex pg-justify-center pg-items-center">
+        <img src="@/assets/svg/gift-of-learning/playgarden-prep.svg" class="pg-h-[180px]">
+      </div>
+
+      <!-- LINKS -->
+      <div class="pg-grid pg-grid-cols-1 pg-gap-2">
+        <a v-for="link in links" :key="link.name" :href="link.href">
+          <span class="pg-inline-block pg-text-lg pg-text-[#606060]">{{ link.name }}</span>
+        </a>
+      </div>
+
+      <!-- ONLINE SCHOOL -->
+      <div class="pg-grid pg-grid-cols-1">
+        <div>Online School</div>
+
+        <a href="mailto:hello@playgardenprep.com">
+          hello@playgardenprep.com
+        </a>
+
+        646-504-4716
+      </div>
+    </div>
   </v-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup() {}
+  setup() {
+    const links = ref<{ name: string, href: string }[]>([
+      { name: 'Playgarden Prep', href: '#' },
+      { name: 'Preschool', href: '#' },
+      { name: 'Terms of Use', href: '#' },
+      { name: 'Privacy Policy', href: '#' },
+      { name: 'Help', href: '#' }
+    ])
+
+    return { links }
+  }
 })
 </script>
