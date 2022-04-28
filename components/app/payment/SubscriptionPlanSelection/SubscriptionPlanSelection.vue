b7cc9e676a80fa09ab4576e7db17d33243efb7aa
christiangoldmediatech <69595015+christiangoldmediatech@users.noreply.github.com>
2022-04-28 16:48:23 -0500

    Adding cancel trial

--- components/app/payment/SubscriptionPlanSelection/SubscriptionPlanSelection.vue
+++ components/app/payment/SubscriptionPlanSelection/SubscriptionPlanSelection.vue
@@ -164,7 +164,7 @@
 </template>
 
 <script>
-import { defineComponent, useStore, computed } from '@nuxtjs/composition-api'
+import { defineComponent, useStore, computed, useRoute } from '@nuxtjs/composition-api'
 import { get } from 'lodash'
 import { mapActions, mapGetters } from 'vuex'
 
@@ -205,6 +205,7 @@ export default defineComponent({
   },
 
   setup () {
+    const route = useRoute()
     const store = useStore()
     const { showContactUsModal } = useGlobalModal({ store })
     const { setIsTrialEndingPlanSelectedModalVisible } = useNotification({ store })
@@ -214,9 +215,12 @@ export default defineComponent({
     const Auth = useAuth({ store })
     const Billing = useBilling()
 
+    const fromPlaydates = computed(() => (route.value.query.fromPlaydates === 'true'))
+
     return {
       Auth,
       Billing,
+      fromPlaydates,
       showContactUsModal,
       isAnnualSubscriptionEnabled,
       setIsTrialEndingPlanSelectedModalVisible
@@ -338,8 +342,9 @@ export default defineComponent({
             return
           }
         }
-
+        plan.fromPlaydates = this.fromPlaydates
         await this.selectSubscriptionPlan(plan)
+        await this.Auth.fetchUserInfo()
         this.setIsTrialEndingPlanSelectedModalVisible(true)
         this.$nuxt.$emit('plan-membership-changed')
         this.$emit('click:submit', {
