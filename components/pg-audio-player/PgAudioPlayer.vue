7d537841b773370e1bb7563f0541a851b7b4fdc1
Esteban <esteban.lds@outlook.com>
2022-05-03 22:04:46 -0500

    [update] prevent play on empty song

--- components/pg-audio-player/PgAudioPlayer.vue
+++ components/pg-audio-player/PgAudioPlayer.vue
@@ -161,7 +161,7 @@ export default defineComponent({
       this.isPaused = false
       this.isPlaying = false
     },
-    play () {
+    async play () {
       if (this.isLoading) {
         return
       }
@@ -170,7 +170,7 @@ export default defineComponent({
         return
       }
       if (!this.isPlaying) {
-        this.player.play()
+        await this.player.play()
         this.isPlaying = true
         this.isPaused = false
         this.isStopped = false
@@ -227,8 +227,16 @@ export default defineComponent({
       }
       this.isLoading = true
       const selectingSong = this.currentPlaylist[songIndex]
+
+      // stop if there is no song
+      if (!selectingSong) {
+        this.isLoading = false
+        return
+      }
+
       // Abort current track
       this.player.load()
+
       // Update state
       this.currentSong = selectingSong
       this.currentSongIndex = songIndex
