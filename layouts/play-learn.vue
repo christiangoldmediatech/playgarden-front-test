36f4d71dbc8753d655fa07991138146c156dbc27
Esteban <esteban.lds@outlook.com>
2022-05-11 19:25:57 -0500

    [update] layouts

---
+++ layouts/play-learn.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <!-- TOOLBAR -->
+    <Toolbar @input:open-drawer="openDrawer" />
+
+    <!-- BODY -->
+    <v-main>
+      <v-container fluid class="pg-mx-auto pg-max-w-[1400px]">
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <!-- FOOTER -->
+    <Footer />
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Footer from '@/components/app/gift-of-learning/Footer/Footer.vue'
+import Toolbar from '@/components/app/learn-play/Toolbar/Toolbar.vue'
+
+export default defineComponent({
+  name: 'LayoutsPlayLearn',
+
+  middleware: ['utmHandler'],
+
+  components: {
+    Toolbar,
+    Footer
+  },
+
+  setup () {}
+})
+</script>
