f3c2408844ac3784cc98fa8a9c7f9dbf0b97d583
Esteban <esteban.lds@outlook.com>
2022-05-12 21:47:19 -0500

    [add] mobile support for info card

--- components/app/learn-play/InfoCard/InfoCard.vue
+++ components/app/learn-play/InfoCard/InfoCard.vue
@@ -5,25 +5,32 @@
         'pg-flex',
         'pg-flex-col',
         'pg-py-6',
-        'pg-px-14',
+        'pg-px-8',
         'pg-text-center',
         'pg-shadow-[0px_8px_24px_rgba(0,0,0,0.15)]',
-        'pg-h-[500px]',
-        'pg-w-[580px]',
         'pg-rounded-lg',
-        'pg-bg-white'
+        'pg-bg-white',
+        'pg-max-w-[500px]',
+        'lg:pg-px-10',
+        'lg:pg-w-[500px]',
+        'lg:pg-h-[500px]',
       ]"
     >
       <!-- TITLE -->
       <div>
-        <underlined-title text="Play and Learn for family fun and well-being!" font-size="28px" />
+        <underlined-title
+          text="Play and Learn for family fun and well-being!"
+          font-size="28px"
+          font-size-mobile="24px"
+        />
       </div>
 
       <!-- CONTENT -->
-      <div class="pg-text-xl pg-text-black pg-mt-4">
+      <div class="pg-text-base lg:pg-text-xl pg-text-black pg-mt-4">
         Start learning together now, cancel any time!
       </div>
 
+      <!-- IMAGE -->
       <div
         :class="[
           'pg-relative',
@@ -34,12 +41,20 @@
       >
         <img
           src="@/assets/png/play-learn/acuarela-activity.png"
-          class="pg-mt-10 pg-z-10"
+          class="pg-mt-8 lg:pg-mt-10 pg-z-[4] pg-h-[150px] lg:pg-h-[250px]"
         >
 
         <img
+          :class="[
+            'pg-absolute',
+            'pg-left-0',
+            'pg-right-0',
+            'pg-top-0',
+            'pg-mx-auto',
+            'pg-h-[200px]',
+            'lg:pg-h-[350px]'
+          ]"
           src="@/assets/png/play-learn/acuarela-green.png"
-          class="pg-absolute pg-top-[-16px] pg-left-0 pg-right-0"
         >
       </div>
     </div>
