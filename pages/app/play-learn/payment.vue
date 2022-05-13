c78c41826334c666eda9b0356516ceef358df1f3
Esteban <esteban.lds@outlook.com>
2022-05-12 22:22:43 -0500

    wip payment

---
+++ pages/app/play-learn/payment.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Play & Learn Payment</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppPlayLearnPayment'
+})
+</script>
