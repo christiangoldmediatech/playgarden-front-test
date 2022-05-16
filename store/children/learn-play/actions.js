cd36f06ac560b3bbc69376da2d2d66e40c4eb828
jorgestg <jorgepenn86@gmail.com>
2022-05-16 12:04:32 -0400

    change(play-and-learn): use new endpoint

--- store/children/learn-play/actions.js
+++ store/children/learn-play/actions.js
@@ -1,19 +1,17 @@
 import { snotifyError } from '@/utils/vuex'
 
 export default {
+  getFirstLearnPlay({ commit }, childId) {
+    if (!childId) {
+      return
+    }
 
-  getFirstLearnPlay ({ commit }, params) {
-    return new Promise((resolve, reject) =>
-      this.$axios
-        .$get('/play-and-learn/get/current', { params })
-        .then(resolve)
-        .catch((error) => {
-          snotifyError(commit, {
-            body: 'Sorry! There was an error while getting learn-play.'
-          })
-          // TO DO async away
-          reject(error)
-        })
-    )
+    return this.$axios.$get(`/play-and-learn/by-child/${childId}`).catch((error) => {
+      snotifyError(commit, {
+        body: 'Sorry! There was an error while getting learn-play.'
+      })
+
+      throw error
+    })
   }
 }
