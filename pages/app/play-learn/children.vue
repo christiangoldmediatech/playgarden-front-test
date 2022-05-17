2a1e1fee0030e51baf8772c60da47d10a1c3c7f5
Esteban <esteban.lds@outlook.com>
2022-05-17 01:31:59 -0500

    [wip] children page

---
+++ pages/app/play-learn/children.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Play & Learn Children</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppPlayLearnChildren',
+
+  setup() {}
+})
+</script>
