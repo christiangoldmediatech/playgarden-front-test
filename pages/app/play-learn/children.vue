535e0c7b6cecbfd483e7b70c5797808e840ba2ab
Esteban <esteban.lds@outlook.com>
2022-05-17 09:50:32 -0500

    [add] children page

--- pages/app/play-learn/children.vue
+++ pages/app/play-learn/children.vue
@@ -1,13 +1,124 @@
 <template>
-  <div>Play & Learn Children</div>
+  <div class="lg:pg-pb-32">
+    <div
+      :class="[
+        'pg-flex',
+        'pg-flex-col',
+        'pg-mx-auto',
+        'pg-max-w-[768px]',
+        'pg-px-4',
+        'lg:pg-max-w-[1200px]',
+      ]"
+    >
+      <div class="pg-my-6">
+        <BackButton @click="handleGoBack" />
+      </div>
+
+      <!-- CONTENT -->
+      <div
+        :class="[
+          'pg-grid',
+          'pg-grid-cols-1',
+          'lg:pg-mt-12',
+          'lg:pg-grid-cols-12',
+          'lg:pg-gap-12',
+        ]"
+      >
+        <!-- LEFT -->
+        <div class="pg-col-span-full lg:pg-col-span-5">
+          <img
+            src="@/assets/png/play-learn/child-with-backpack.png"
+            class="pg-w-full"
+          >
+        </div>
+
+        <!-- RIGHT -->
+        <div
+          :class="[
+            'pg-col-span-full',
+            'pg-flex',
+            'pg-flex-col',
+            'pg-justify-center',
+            'pg-mt-14',
+            'lg:pg-col-span-7',
+            'lg:pg-mt-0'
+          ]"
+        >
+          <ShortRegisterForm
+            :loading="isLoading"
+            @click:submit="handleSubmit"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useContext, useRouter, useStore } from '@nuxtjs/composition-api'
+import BackButton from '@/components/app/learn-play/BackButton/BackButton.vue'
+import ShortRegisterForm from '@/components/forms/children/ShortRegisterForm.vue'
+import { useAuth, useChild, usePayment, useSnotifyHelper } from '@/composables'
+import { CardData, Child, DataSubscription, TypedStore } from '@/models'
 
 export default defineComponent({
   name: 'AppPlayLearnChildren',
 
-  setup() {}
+  layout: 'play-learn',
+
+  components: {
+    BackButton,
+    ShortRegisterForm
+  },
+
+  setup() {
+    const snotify = useSnotifyHelper()
+    const store = useStore<TypedStore>()
+    const router = useRouter()
+    const Auth = useAuth({ store })
+    const Child = useChild({ store })
+
+    const isLoading = ref(false)
+    const toggleInfo = ref(true)
+
+    function handleGoBack() {
+      router.go(-1)
+    }
+
+    async function handleSubmit(children: Child[]) {
+      try {
+        isLoading.value = true
+
+        await Promise.all(
+          children.map((child) => {
+            return child.id
+              ? Child.update(child.id, child)
+              : Child.create(child)
+          })
+        )
+
+        await Auth.fetchUserInfo()
+        snotify.success('Child information has been saved!')
+        goToNextStep()
+      } catch {
+        snotify.error('Something went wrong. Please try again.')
+      } finally {
+        isLoading.value = false
+      }
+    }
+
+    function goToNextStep() {
+      router.push({
+        name: 'app-learn-play'
+      })
+    }
+
+    return {
+      isLoading,
+      toggleInfo,
+      handleSubmit,
+      handleGoBack
+    }
+  }
 })
 </script>
