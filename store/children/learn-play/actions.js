25dbf564608d1ae1e0acef7aeb8788aab88ab739
jorgestg <jorgepenn86@gmail.com>
2022-05-18 10:47:22 -0400

    change(play-and-learn): .catch to try-catch block with await

--- store/children/learn-play/actions.js
+++ store/children/learn-play/actions.js
@@ -1,17 +1,19 @@
 import { snotifyError } from '@/utils/vuex'
 
 export default {
-  getFirstLearnPlay({ commit }, childId) {
+  async getFirstLearnPlay({ commit }, childId) {
     if (!childId) {
       return
     }
 
-    return this.$axios.$get(`/play-and-learn/by-child/${childId}`).catch((error) => {
+    try {
+      return await this.$axios.$get(`/play-and-learn/by-child/${childId}`)
+    } catch (error) {
       snotifyError(commit, {
         body: 'Sorry! There was an error while getting learn-play.'
       })
 
       throw error
-    })
+    }
   }
 }
