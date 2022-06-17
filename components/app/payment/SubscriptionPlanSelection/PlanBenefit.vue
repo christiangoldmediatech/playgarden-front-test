3e043bf1f1cffef36ab37963d0120bf5d0527c7c
jorgestg <jorgepenn86@gmail.com>
2022-06-17 17:06:59 -0400

    change(plans): new selector design

---
+++ components/app/payment/SubscriptionPlanSelection/PlanBenefit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pg-flex pg-justify-start pg-items-start pg-mb-3">
+    <v-img
+      :src="require(`@/assets/svg/plans/check-${index}.svg`)"
+      alt="Bullet point"
+      max-width="20"
+      class="pg-mr-2"
+    />
+
+    <span class="pg-leading-6">
+      {{ benefit }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'PlanBenefit',
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    benefit: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
