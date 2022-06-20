4dc7a3a802442ac92becf124151bb7522b7f616f
jorgestg <jorgepenn86@gmail.com>
2022-06-20 13:24:45 -0400

    fix(plan-selection): font

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,12 @@ export default {
         rel: 'stylesheet',
         type: 'text/css',
         href:
+          'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
           'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css'
       }
     ],
