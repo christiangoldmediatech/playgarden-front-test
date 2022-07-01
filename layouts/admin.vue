f86a170a9406c15e1182d71d7a93592af87f090c
jorgestg <jorgepenn86@gmail.com>
2022-07-01 11:46:06 -0400

    chore(admin): remove music library

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -203,11 +203,6 @@ export default {
           route: '/admin/recurring-live-sessions-management'
         },
         {
-          icon: 'mdi-volume-high',
-          title: 'Music Library',
-          route: '/admin/music-library'
-        },
-        {
           icon: 'mdi-filmstrip',
           title: 'Kids corner videos',
           route: '/admin/kids-corner-videos'
