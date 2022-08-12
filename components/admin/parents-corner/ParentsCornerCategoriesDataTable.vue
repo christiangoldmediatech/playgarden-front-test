3cc9972fb2a872a7ccc2138f1ec4000e9a949713
Esteban <estebi@goldmediatech.com>
2022-08-11 20:04:15 -0500

    fix: eslint warnings

--- components/admin/parents-corner/ParentsCornerCategoriesDataTable.vue
+++ components/admin/parents-corner/ParentsCornerCategoriesDataTable.vue
@@ -89,7 +89,7 @@ export default {
 
     remove({ id, name }) {
       this.$nuxt.$emit('open-prompt', {
-        title: "Delete parent's corner category?",
+        title: 'Delete parent\'s corner category?',
         message: `Are you sure you want to delete <b>${name}</b>?`,
         action: async () => {
           await this.deleteCategory(id)
