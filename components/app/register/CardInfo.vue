2b4d36ec094a1bf108f99a1614b8d8378ecf257e
Esteban <estebi@goldmediatech.com>
2022-08-11 23:54:04 -0500

    feat: update card info design

--- components/app/register/CardInfo.vue
+++ components/app/register/CardInfo.vue
@@ -1,45 +1,65 @@
 <template>
-  <div>
-    <div class="text-center text-button font-weight-bold mb-n4">
+  <div
+    :class="[
+      'pg-flex',
+      'pg-flex-col',
+      'pg-py-6',
+      'pg-px-8',
+      'pg-text-center',
+      'pg-shadow-[0px_8px_24px_rgba(0,0,0,0.15)]',
+      'pg-rounded-lg',
+      'pg-bg-white',
+      'pg-max-w-[500px]',
+      'lg:pg-px-10',
+      'lg:pg-w-[500px]',
+      'lg:pg-h-[500px]'
+    ]"
+  >
+    <!-- TITLE -->
+    <div>
       <template>
-        <underlined-title
-          class="text-h6 text-md-h5 ml-sm-4"
+        <UnderlinedTitle
           text="Give your child an immersive educational experience at home!"
+          font-size="24px"
+          font-size-mobile="20px"
         />
       </template>
     </div>
-    <v-card-text>
-      <div>
-        <center class="text-subtitle-1 mt-3 mb-3 aling-spacing">
-          Start learning now; cancel any time!
-        </center>
-        <img
-          src="@/assets/png/login-card-child.png"
-          class="d-block mx-auto child-login"
-        >
-      </div>
-    </v-card-text>
+
+    <!-- CONTENT -->
+    <div class="pg-text-base lg:pg-text-xl pg-text-black pg-mt-4">
+      Start learning now; cancel any time!
+    </div>
+
+    <!-- IMAGE -->
+    <div
+      :class="[
+        'pg-w-full',
+        'pg-flex',
+        'pg-justify-center'
+      ]"
+    >
+      <img
+        src="@/assets/png/login-card-child.png"
+        class="child-login pg-mt-8 lg:pg-mt-10 pg-z-[4] pg-h-[150px] lg:pg-h-[250px]"
+      >
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
   name: 'CardInfo'
-}
+})
 </script>
 
 <style lang="scss" scoped>
 .child-login {
   border-radius: 50%;
-  width: 50%;
   background-color: #b2e68d;
   border: 7px solid #b2e68d;
   outline: 7px solid #68c453;
-}
-.aling-spacing {
-  text-align: center;
-  text-align-last: center;
-  letter-spacing: 1px !important;
-  font-size: 16.6px !important;
 }
 </style>
