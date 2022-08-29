b3b440a7973a3db07fe9b8aef77f8ab033a09a0f
Christian Borja <christian@goldmediatech.com>
2022-08-29 14:45:05 -0500

    applying changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -167,7 +167,7 @@ export default defineComponent({
       watch(
         isUserLoggedIn,
         async () => {
-          if (routeName.value !== 'shared-slug' && isUserLoggedIn) {
+          if (routeName.value !== 'shared-slug' && isUserLoggedIn.value) {
             await checkIfShouldSendShippingAddressNotification()
             await checkIfShouldShowTrialExpiredModal()
             await checkIfShouldShowTrialExpiringRibbon()
