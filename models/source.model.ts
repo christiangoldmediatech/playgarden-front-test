04cc654f2b52b91689243825ae0604f0b7a7df8c
Christian Borja <christian@goldmediatech.com>
2022-10-18 22:01:07 -0500

    Adding utm source

---
+++ models/source.model.ts
@@ -0,0 +1,11 @@
+export interface UserSource {
+   // eslint-disable-next-line camelcase
+   utm_source: string | null
+   // eslint-disable-next-line camelcase
+   utm_medium: string | ''
+   // eslint-disable-next-line camelcase
+   utm_campaign: string | ''
+   // eslint-disable-next-line camelcase
+   utm_name: string | ''
+   gc1Id: string | '' | null
+ }
