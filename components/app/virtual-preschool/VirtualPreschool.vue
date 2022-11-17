ba383210318c702c3f0c4dc08cafd1f033a715e3
Arias-xss <kokiarias01@gmail.com>
2022-11-17 16:11:46 -0300

    Change LiveClasses title on VirtualPreschool

--- components/app/virtual-preschool/VirtualPreschool.vue
+++ components/app/virtual-preschool/VirtualPreschool.vue
@@ -139,7 +139,7 @@ export default defineComponent({
       classes: {
         imageUrl: require('@/assets/png/virtual-preschool/live classes.png'),
         teacherUrl: require('@/assets/png/virtual-preschool/teacher/Miss_Lucy-Liveclasses.png'),
-        title: 'Live Classes',
+        title: 'Live Classes & Playdates',
         route: { name: 'app-live-classes' },
         message: 'Enjoy cooking, music, movement zooms and more!',
         audio: `${baseRoute}audio/virtual-preschool/Live classes 2.m4a`
