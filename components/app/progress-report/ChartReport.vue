f327a2d00cc67aba27e23b3957df082f10f4d9d8
cgarrigoza97 <cgarrigoza@gmail.com>
2023-01-16 16:14:04 -0300

    update to new design

--- components/app/progress-report/ChartReport.vue
+++ components/app/progress-report/ChartReport.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <v-row v-if="isMobile" no-gutters>
+      <v-col cols="4" class="d-flex flex-column align-center justify-center">
+        <div class="pg-w-[50px] pg-h-[15px] pg-rounded-[4px] pg-bg-[#D1ADF5] mb-2"></div>
+        <span class="bar-label-mobile">Progressing</span>
+      </v-col>
+      <v-col cols="4" class="d-flex flex-column align-center justify-center">
+        <div class="pg-w-[50px] pg-h-[15px] pg-rounded-[4px] pg-bg-[#FFB8E7] mb-2"></div>
+        <span class="bar-label-mobile">Age Appropriate</span>
+      </v-col>
+      <v-col cols="4" class="d-flex flex-column align-center justify-center">
+        <div class="pg-w-[50px] pg-h-[15px] pg-rounded-[4px] pg-bg-[#A5EA95] mb-2"></div>
+        <span class="bar-label-mobile">Area of Strength</span>
+      </v-col>
+    </v-row>
     <report-chart ref="progress-chart" :graph="chartOptions" @click="handleClick" />
   </div>
 </template>
@@ -24,10 +38,18 @@ export default {
   }),
 
   computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.mdAndDown
+    },
     getCategories () {
       return (this.report.categories) ? this.report.categories : []
     },
     getCategoriesFormatted () {
+      // A  is added at the beginning in order to render and empty space
+      if (this.$vuetify.breakpoint.mdAndDown) {
+        return [...this.getCategories]
+      }
+
       return ['', ...this.getCategories]
     },
     getRotate () {
@@ -55,21 +77,181 @@ export default {
     getIconSize () {
       return (this.$vuetify.breakpoint.xs) ? '45' : '60'
     },
+    getHorizontalLabels() {
+      return this.$vuetify.breakpoint.mdAndDown
+        ? []
+        : [{
+            symbolSize: 20,
+            data: [],
+            type: 'bar',
+            silent: true,
+            markArea: {
+              label: {
+                show: true,
+                position: 'left',
+                distance: this.$vuetify.breakpoint.smAndDown ? -110 : -140,
+                color: '#606060',
+                fontSize: this.getLabelFontSize,
+                formatter: '{square|      }  Area of Strength',
+                rich: {
+                  square: {
+                    fontSize: '18px',
+                    backgroundColor: ' #A5EA95',
+                    borderRadius: 4
+                  }
+                },
+                fontWeight: 'bold',
+                verticalAlign: 'middle',
+                fontFamily: 'Quicksand, Poppins',
+                opacity: 1
+              },
+              itemStyle: {
+                color: 'transparent',
+                borderWidth: 0,
+                borderType: 'none'
+              },
+              data: [[{ yAxis: 90 }, { yAxis: 90 }]]
+            }
+          },
+          {
+            symbolSize: 20,
+            data: [],
+            type: 'bar',
+            silent: true,
+            markArea: {
+              label: {
+                show: true,
+                position: 'left',
+                distance: this.$vuetify.breakpoint.smAndDown ? -110 : -140,
+                color: '#606060',
+                fontSize: this.getLabelFontSize,
+                formatter: '{square|      }  Age Appropriate',
+                rich: {
+                  square: {
+                    fontSize: '18px',
+                    backgroundColor: '#FFB8E7',
+                    borderRadius: 4
+                  }
+                },
+                fontWeight: 'bold',
+                verticalAlign: 'middle',
+                fontFamily: 'Quicksand, Poppins',
+                opacity: 1
+              },
+              itemStyle: {
+                color: 'transparent',
+                borderWidth: 0,
+                borderType: 'none'
+              },
+              data: [[{ yAxis: 48 }, { yAxis: 48 }]]
+            }
+          },
+          {
+            symbolSize: 20,
+            data: [],
+            type: 'bar',
+            silent: true,
+            markArea: {
+              label: {
+                show: true,
+                position: 'left',
+                distance: this.$vuetify.breakpoint.smAndDown ? -90 : -100,
+                color: '#606060',
+                fontSize: this.getLabelFontSize,
+                fontWeight: 'bold',
+                formatter: '{square|      }  Progressing',
+                rich: {
+                  square: {
+                    fontSize: '18px',
+                    backgroundColor: '#D1ADF5',
+                    borderRadius: 4
+                  }
+                },
+                verticalAlign: 'middle',
+                fontFamily: 'Quicksand, Poppins',
+                opacity: 1
+              },
+              itemStyle: {
+                color: 'transparent',
+                borderWidth: 0,
+                borderType: 'none'
+              },
+              data: [[{ yAxis: 10 }, { yAxis: 10 }]]
+            }
+          }]
+    },
     getDataSeriesWithStyles() {
-      let cognitive = this.report.dataSerie.find((data) => data.nameCardType === 'Cognitive')
-      let languageAndLiteracy = this.report.dataSerie.find((data) => data.nameCardType === 'Language & Literacy')
-      let physical = this.report.dataSerie.find((data) => data.nameCardType === 'Physical')
-      let socialAndEmotional = this.report.dataSerie.find((data) => data.nameCardType === 'Social and Emotional')
+      const cognitive = this.report.dataSerie.find((data) => data.nameCardType === 'Cognitive')
+      const languageAndLiteracy = this.report.dataSerie.find((data) => data.nameCardType === 'Language & Literacy')
+      const physical = this.report.dataSerie.find((data) => data.nameCardType === 'Physical')
+      const socialAndEmotional = this.report.dataSerie.find((data) => data.nameCardType === 'Social and Emotional')
 
-      // In case the percentage is 0, we set the value to 1 so a little bar is rendered
-      cognitive = { ...cognitive, value: (cognitive && cognitive.value) ? cognitive.value : 1, itemStyle: { color: '#FA8792', borderRadius: '40%' } }
-      languageAndLiteracy = { ...languageAndLiteracy, value: (languageAndLiteracy && languageAndLiteracy.value) ? languageAndLiteracy.value : 1, itemStyle: { color: '#78C383', borderRadius: '40%' } }
-      physical = { ...physical, value: (physical && physical.value) ? physical.value : 1, itemStyle: { color: '#69BEFF', borderRadius: '40%' } }
-      socialAndEmotional = { ...socialAndEmotional, value: (socialAndEmotional && socialAndEmotional.value) ? socialAndEmotional.value : 1, itemStyle: { color: '#FFAF4B', borderRadius: '40%' } }
+      const separatedCognitive = this.separateValues(cognitive)
+      const separatedLanguageAndLiteracy = this.separateValues(languageAndLiteracy)
+      const separatedPhysical = this.separateValues(physical)
+      const separatedSocialAndEmotional = this.separateValues(socialAndEmotional)
 
-      const dataSeriesPadding = { value: 0, nameCardType: '' }
+      const firstSeparationValues = [separatedCognitive[0], separatedLanguageAndLiteracy[0], separatedPhysical[0], separatedSocialAndEmotional[0]]
+      const secondSeparationValues = [separatedCognitive[1], separatedLanguageAndLiteracy[1], separatedPhysical[1], separatedSocialAndEmotional[1]]
+      const thirdSeparationValues = [separatedCognitive[2], separatedLanguageAndLiteracy[2], separatedPhysical[2], separatedSocialAndEmotional[2]]
 
-      return [dataSeriesPadding, cognitive, languageAndLiteracy, physical, socialAndEmotional]
+      // A zero is added at the beginning in order to render and empty space
+      if (!this.$vuetify.breakpoint.mdAndDown) {
+        firstSeparationValues.unshift(0)
+        secondSeparationValues.unshift(0)
+        thirdSeparationValues.unshift(0)
+      }
+
+      const label = {
+        show: true,
+        position: 'top',
+        formatter (val) {
+          const showLabel = val.data.showLabel
+          const value = val.data.realValue ? val.data.realValue : val.value
+          if (value <= 1 || !showLabel) {
+            return ''
+          }
+
+          return `${value} %`
+        },
+        color: '#606060',
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-SemiBold, Poppins',
+        fontSize: this.getValueFontSize
+      }
+
+      const firstSeparation = {
+        name: 'separation1',
+        data: firstSeparationValues,
+        itemStyle: { color: '#D1ADF5' },
+        type: 'bar',
+        stack: 'ad',
+        barWidth: '80%',
+        barCategoryGap: '10%',
+        label: { ...label }
+      }
+      const secondSeparation = {
+        name: 'separation2',
+        data: secondSeparationValues,
+        stack: 'ad',
+        itemStyle: { color: '#FFB8E7' },
+        type: 'bar',
+        barWidth: '80%',
+        barCategoryGap: '10%',
+        label: { ...label }
+      }
+      const thirdSeparation = {
+        name: 'separation3',
+        data: thirdSeparationValues,
+        stack: 'ad',
+        itemStyle: { color: '#A5EA95' },
+        type: 'bar',
+        barWidth: '80%',
+        barCategoryGap: '10%',
+        label: { ...label }
+      }
+
+      return [firstSeparation, secondSeparation, thirdSeparation]
     },
     chartOptions () {
       return {
@@ -122,146 +304,54 @@ export default {
             rotate: this.getRotateYaxios
           }
         },
-        series: [{
-          data: this.getDataSeriesWithStyles,
-          type: 'bar',
-          barWidth: '80%',
-          barCategoryGap: '10%',
-          label: {
-            show: true,
-            position: 'inside',
-            formatter (val) {
-              if (val.value <= 1) {
-                return ''
-              }
-
-              return `${val.value} %`
-            },
-            color: '#FFFFFF',
-            fontWeight: 'bold',
-            fontFamily: 'Poppins-SemiBold, Poppins',
-            fontSize: this.getValueFontSize
-          }
-        },
-        {
-          symbolSize: 20,
-          silent: true,
-          data: [],
-          type: 'bar',
-          markArea: {
-            itemStyle: {
-              borderWidth: 1,
-              borderType: 'solid',
-              borderColor: '#CCCCCC'
-            },
-            data: [[{ yAxis: 20 }, { yAxis: 20 }]]
-          }
-        },
-        {
-          symbolSize: 20,
-          silent: true,
-          data: [],
-          type: 'bar',
-          markArea: {
-            itemStyle: {
-              color: '#68d2e0',
-              borderWidth: 1,
-              borderType: 'solid',
-              borderColor: '#CCCCCC'
-            },
-            data: [[{ yAxis: 80 }, { yAxis: 80 }]]
-          }
-        },
-        {
-          symbolSize: 20,
-          silent: true,
-          data: [],
-          type: 'bar',
-          markArea: {
-            itemStyle: {
-              color: '#000000',
-              borderWidth: 1,
-              borderType: 'solid',
-              borderColor: '#CCCCCC'
-            },
-            data: [[{ yAxis: 100 }, { yAxis: 100 }]]
-          }
-        },
-        {
-          symbolSize: 20,
-          data: [],
-          type: 'bar',
-          silent: true,
-          markArea: {
-            label: {
-              show: true,
-              position: 'left',
-              distance: this.$vuetify.breakpoint.smAndDown ? -110 : -155,
-              color: '#606060',
-              fontSize: this.getLabelFontSize,
-              fontWeight: 'bold',
-              verticalAlign: 'middle',
-              fontFamily: 'Quicksand, Poppins',
-              opacity: 1
-            },
-            itemStyle: {
-              color: 'transparent',
-              borderWidth: 0,
-              borderType: 'none'
-            },
-            data: [[{ name: 'Area of Strength', yAxis: 90 }, { yAxis: 90 }]]
-          }
-        },
-        {
-          symbolSize: 20,
-          data: [],
-          type: 'bar',
-          silent: true,
-          markArea: {
-            label: {
-              show: true,
-              position: 'left',
-              distance: this.$vuetify.breakpoint.smAndDown ? -110 : -150,
-              color: '#606060',
-              fontSize: this.getLabelFontSize,
-              fontWeight: 'bold',
-              verticalAlign: 'middle',
-              fontFamily: 'Quicksand, Poppins',
-              opacity: 1
-            },
-            itemStyle: {
-              color: 'transparent',
-              borderWidth: 0,
-              borderType: 'none'
-            },
-            data: [[{ name: 'Age Appropriate', yAxis: 48 }, { yAxis: 48 }]]
-          }
-        },
-        {
-          symbolSize: 20,
-          data: [],
-          type: 'bar',
-          silent: true,
-          markArea: {
-            label: {
-              show: true,
-              position: 'left',
-              distance: this.$vuetify.breakpoint.smAndDown ? -90 : -100,
-              color: '#606060',
-              fontSize: this.getLabelFontSize,
-              fontWeight: 'bold',
-              verticalAlign: 'middle',
-              fontFamily: 'Quicksand, Poppins',
-              opacity: 1
-            },
-            itemStyle: {
-              color: 'transparent',
-              borderWidth: 0,
-              borderType: 'none'
-            },
-            data: [[{ name: 'Progressing', yAxis: 10 }, { yAxis: 10 }]]
-          }
-        }]
+        series: [
+          ...this.getDataSeriesWithStyles,
+          {
+            symbolSize: 20,
+            silent: true,
+            data: [],
+            type: 'bar',
+            markArea: {
+              itemStyle: {
+                borderWidth: 1,
+                borderType: 'solid',
+                borderColor: '#CCCCCC'
+              },
+              data: [[{ yAxis: 20 }, { yAxis: 20 }]]
+            }
+          },
+          {
+            symbolSize: 20,
+            silent: true,
+            data: [],
+            type: 'bar',
+            markArea: {
+              itemStyle: {
+                color: '#68d2e0',
+                borderWidth: 1,
+                borderType: 'solid',
+                borderColor: '#CCCCCC'
+              },
+              data: [[{ yAxis: 80 }, { yAxis: 80 }]]
+            }
+          },
+          {
+            symbolSize: 20,
+            silent: true,
+            data: [],
+            type: 'bar',
+            markArea: {
+              itemStyle: {
+                color: '#000000',
+                borderWidth: 1,
+                borderType: 'solid',
+                borderColor: '#CCCCCC'
+              },
+              data: [[{ yAxis: 100 }, { yAxis: 100 }]]
+            }
+          },
+          ...this.getHorizontalLabels
+        ]
       }
     }
   },
@@ -271,12 +361,60 @@ export default {
   created () {},
 
   methods: {
+    separateValues(dataSeries) {
+      /*
+        Values should be separated in three in order to render three stack bars
+      */
+
+      // In case the percentage is 0, we set the value to 1 so a little bar is rendered
+      const value = (dataSeries && dataSeries.value) ? dataSeries.value : 1
+      const getDataObject = (dataValue, showLabel) => ({ ...dataSeries, value: dataValue, showLabel, realValue: value })
+
+      const firstUpperLimit = 20
+      const secondUpperLimit = 80
+
+      if (value <= firstUpperLimit) {
+        return [
+          getDataObject(value, true),
+          getDataObject(0, false),
+          getDataObject(0, false)
+        ]
+      } else if (value <= secondUpperLimit) {
+        return [
+          getDataObject(firstUpperLimit, false),
+          getDataObject(value - firstUpperLimit, true),
+          getDataObject(0, false)
+        ]
+      } else {
+        return [
+          getDataObject(firstUpperLimit, false),
+          getDataObject(secondUpperLimit - firstUpperLimit, false),
+          getDataObject(value - secondUpperLimit, true)
+        ]
+      }
+    },
     pointClick (event) {
       this.$nuxt.$emit('detail-progress-report', event)
     },
     handleClick (val) {
-      this.$emit('click', val)
+      const valCopy = { ...val }
+      if ('realValue' in valCopy.data) {
+        valCopy.data = { ...valCopy.data, value: valCopy.data.realValue }
+      }
+
+      this.$emit('click', valCopy)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.bar-label-mobile {
+  // font-family: 'Quicksand';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 11px;
+  line-height: 14px;
+  color: #606060;
+}
+</style>
