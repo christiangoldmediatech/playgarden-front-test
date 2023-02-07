d575bbfdb2a5084ce7d25aefe1ac395b53b82e30
Arias-xss <kokiarias01@gmail.com>
2023-02-07 20:06:14 -0300

    Fit height

--- layouts/iframe-header.vue
+++ layouts/iframe-header.vue
@@ -3,7 +3,11 @@
     <iframe
       src="https://playgardenonline.com/empty-header?header-iframe=true"
       height="250"
+      @mouseenter="onMouseIn"
+      @mouseleave="onMouseLeave"
     />
+
+    <div class="pg-h-32"></div>
 
     <v-main>
       <v-container class="pa-0">
@@ -20,12 +24,25 @@ import DefaultFooter from '@/components/app/footer/DefaultFooter.vue'
 
 export default {
   name: 'IframeHeader',
-  components: { DefaultFooter }
+  components: { DefaultFooter },
+  methods: {
+    onMouseIn(e) {
+      e.target.style.height = '100vh'
+    },
+    onMouseLeave(e) {
+      e.target.style.height = ''
+    }
+  }
 }
 </script>
 
 <style scoped>
 iframe {
   border: unset;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
 }
 </style>
