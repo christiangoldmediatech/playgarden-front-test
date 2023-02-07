849432923213b928923cc549ebe796a54111e241
Arias-xss <kokiarias01@gmail.com>
2023-02-06 23:15:01 -0300

    Forgot password - WordPress header

---
+++ layouts/iframe-header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <iframe
+      src="https://playgardenonline.com/empty-header?header-iframe=true"
+      height="250"
+    />
+
+    <v-main>
+      <v-container class="pa-0">
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <default-footer />
+  </v-app>
+</template>
+
+<script>
+import DefaultFooter from '@/components/app/footer/DefaultFooter.vue'
+
+export default {
+  name: 'IframeHeader',
+  components: { DefaultFooter }
+}
+</script>
+
+<style scoped>
+iframe {
+  border: unset;
+}
+</style>
