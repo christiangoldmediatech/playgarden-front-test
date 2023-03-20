017bb7db13cfcfabd8a42fb3a708870f3de00b71
Arias-xss <kokiarias01@gmail.com>
2023-03-20 12:17:53 -0300

    Fix lint problems

--- components/app/header/computed.js
+++ components/app/header/computed.js
@@ -96,7 +96,7 @@ export default {
               title: 'Community',
               dataTestId: 'app-community',
               external: true,
-              link: `https://community.playgardenprep.com/login?atoken=${this.getAccessToken}`,
+              link: `https://community.playgardenprep.com/login/?atoken=${this.getAccessToken}`,
               openInNewTab: true
             }
             // {
