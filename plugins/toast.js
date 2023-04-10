c6eb78e492cbb89c49ac5f831044827945a8ccde
Arias-xss <kokiarias01@gmail.com>
2023-04-10 15:48:17 -0400

    Change design of toast

--- plugins/toast.js
+++ plugins/toast.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
-import Toast, { POSITION } from 'vue-toastification'
+import Toast, { POSITION, TYPE } from 'vue-toastification'
 
 import 'vue-toastification/dist/index.css'
+import '@/assets/css/toast.css'
 
-Vue.use(Toast, { position: POSITION.TOP_RIGHT })
+Vue.use(Toast, {
+  position: POSITION.TOP_RIGHT,
+  hideProgressBar: true,
+  icon: false,
+  pauseOnFocusLoss: false,
+  toastDefaults: {
+    [TYPE.SUCCESS]: {
+      containerClassName: 'pg-toast-container',
+      bodyClassName: ['pg-toast-success', 'pg-toast'],
+      closeButtonClassName: ['pg-toast-close', 'pg-toast-close-success']
+    },
+    [TYPE.ERROR]: {
+      containerClassName: 'pg-toast-container',
+      bodyClassName: ['pg-toast-error', 'pg-toast'],
+      closeButtonClassName: ['pg-toast-close', 'pg-toast-close-error']
+    },
+    [TYPE.WARNING]: {
+      containerClassName: 'pg-toast-container',
+      bodyClassName: ['pg-toast-warning', 'pg-toast'],
+      closeButtonClassName: ['pg-toast-close', 'pg-toast-close-warning']
+    }
+  }
+}
+)
