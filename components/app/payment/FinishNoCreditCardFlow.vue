7f6bffd5599d1568403968456adbebf99d1aa67d
Jesse I. Angell <jesse.angell.1990@gmail.com>
2023-09-21 20:29:17 -0600

    Adds translations to component

--- components/app/payment/FinishNoCreditCardFlow.vue
+++ components/app/payment/FinishNoCreditCardFlow.vue
@@ -25,20 +25,20 @@
     <v-col cols="12" class="pa-0">
       <v-row no-gutters justify="center">
         <h1 class="pg-text-2xl pg-text-center pg-text-[#68C453]">
-          Are you enjoying Playgarden Online?
+          {{ $t('title') }}
         </h1>
       </v-row>
     </v-col>
 
     <v-col cols="12" class="px-10 py-0">
       <p class="pg-text-[#606060] pg-font-bold pg-my-5 pg-text-center">
-        If you liked the first few days, select a plan to continue learning with your little one.
+        {{ $t('text') }}
       </p>
     </v-col>
 
     <v-col cols="12" class="pg-my-5">
       <v-btn color="accent" class="!pg-mx-auto !pg-block !pg-w-fit" @click="$router.push({name: 'app-payment-plan'})">
-        VIEW PLANS
+        {{ $t('viewPlans') }}
       </v-btn>
     </v-col>
 
@@ -76,4 +76,4 @@ export default defineComponent({
 })
 </script>
 
-<style></style>
+<i18n src="@/lang/etc/FinishNoCreditCardFlow.locales.json"></i18n>
