d4921b60dcf0d767f76ba1c50cc484e9798551db
cgarrigoza97 <cgarrigoza@gmail.com>
2023-10-05 14:45:05 -0300

    fix

--- components/app/payment/CancellationSteps.vue
+++ components/app/payment/CancellationSteps.vue
@@ -339,7 +339,7 @@ export default defineComponent({
             await applyDiscountCode(discountCode.value, props.reasonMessage, cancellationExplanation.value)
             viewFirstPositiveModal.value = true
           } else {
-            await handleLastAction(data)
+            await applySubscriptionCancelLogic()
           }
 
           return
