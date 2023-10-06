44d5bafca76825e14e23be7c154f7afafb7b434e
Jesse I. Angell <jesse.angell.1990@gmail.com>
2023-10-06 14:24:13 -0600

    Remove question from questionnaire

--- components/questionnaire/QuestionnaireQuestion3.vue
+++ components/questionnaire/QuestionnaireQuestion3.vue
@@ -12,44 +12,25 @@
       <div
         class="questionnaire-answers questionnaire-answers--radio pg-mb-5 lg:pg-mb-16"
       >
-        <v-radio-group
-          v-model="questDataState.question3.answer"
-          hide-details
-          class="pg-mt-0 !pg-mb-10"
-        >
+        <v-radio-group v-model="questDataState.question3.answer" hide-details>
           <div
             class="pg-mb-4 pg-text-[#606060] pg-font-bold pg-text-base pg-leading-[20px]"
           >
-            {{ $t('question3.text') }}
-          </div>
-          <v-radio
-            v-for="(option, index) in question3Options"
-            :key="`question-3-option-${index + 1}`"
-            off-icon="mdi-radiobox-blank primary--text"
-            :label="option"
-            :value="question3OptionsValues[index]"
-          />
-        </v-radio-group>
-
-        <v-radio-group v-model="questDataState.question4.answer" hide-details>
-          <div
-            class="pg-mb-4 pg-text-[#606060] pg-font-bold pg-text-base pg-leading-[20px]"
-          >
-            {{ $t('question4.textComp') }}
+            {{ $t('question3.textComp') }}
             <a
               class="warning--text !pg-underline"
               target="_blank"
               href="https://www.cognitivetoybox.com/"
             >
-              {{ $t('question4.cognitiveToybox') }}
+              {{ $t('question3.cognitiveToybox') }}
             </a>?
           </div>
           <v-radio
-            v-for="(option, index) in question4Options"
+            v-for="(option, index) in question3Options"
             :key="`question-4-option-${index + 1}`"
             off-icon="mdi-radiobox-blank primary--text"
             :label="option"
-            :value="question4OptionsValues[index]"
+            :value="question3OptionsValues[index]"
           />
         </v-radio-group>
       </div>
@@ -61,7 +42,7 @@
           depressed
           :loading="isSaving"
           :disabled="
-            !questDataState.question3.answer || !questDataState.question4.answer
+            !questDataState.question3.answer
           "
           @click="onSave"
         >
@@ -106,14 +87,6 @@ export default defineComponent({
       return language.t('question3.options', 'en', undefined) as unknown as Array<string>
     })
 
-    const question4Options = computed(() => {
-      return language.t('question4.options') as unknown as Array<string>
-    })
-
-    const question4OptionsValues = computed(() => {
-      return language.t('question4.options', 'en', undefined) as unknown as Array<string>
-    })
-
     let startTime = dayjs()
     onMounted(() => {
       startTime = dayjs()
@@ -134,8 +107,6 @@ export default defineComponent({
       isSaving,
       question3Options,
       question3OptionsValues,
-      question4Options,
-      question4OptionsValues,
       onSave
     }
   }
